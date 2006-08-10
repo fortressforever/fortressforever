a351a36deacd5b33d61ee8da11e8f32967faf6bc
mirvin_monkey <mirvin_monkey@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2006-08-10 19:23:07 +0000

    Viewmodel muzzle flash fix

--- cl_dll/c_te_legacytempents.cpp
+++ cl_dll/c_te_legacytempents.cpp
@@ -2829,7 +2829,7 @@ void CTempEnts::MuzzleFlash_Shotgun_Player( ClientEntityHandle_t hEntity, int at
 			return;
 
 		pParticle->m_flLifetime		= 0.0f;
-		pParticle->m_flDieTime		= 0.0001f;
+		pParticle->m_flDieTime		= 0.001f;	// |-- Mirv: Last a bit longer
 
 		pParticle->m_vecVelocity.Init();
 
