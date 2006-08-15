47173bd4b6f1c3ed182082ee96537eae516843cf
mulchman <mulchman@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2006-08-15 20:19:32 +0000

    reverting this back... it's going to mess up speed effects set by lua when carrying flags. will do a different fix later.

--- dlls/ff/ff_player.cpp
+++ dlls/ff/ff_player.cpp
@@ -4879,11 +4879,8 @@ void CFFPlayer::SetDisguise(int iTeam, int iClass, bool bInstant /* = false */)
 
 int CFFPlayer::AddHealth(unsigned int amount)
 {
-	if( amount > 0 )
-		ClearSpeedEffects();
-
 	int left = TakeHealth( amount, DMG_GENERIC );
-    return left;
+    return left;		
 }
 
 //-----------------------------------------------------------------------------
