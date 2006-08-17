daa4cb3d817a247e7aea19594aa120b01c77b7e8
mulchman <mulchman@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2006-08-17 18:34:17 +0000

    hi

---
+++ dlls/ff/ff_mapfilter.cpp
@@ -0,0 +1,65 @@
+// =============== Fortress Forever ==============
+// ======== A modification for Half-Life 2 =======
+//
+// @file ff_mapfilter.cpp
+// @author Patrick O'Leary (Mulchman) 
+// @date 8/17/2006
+// @brief Map filter
+//
+// REVISIONS
+// ---------
+//	8/17/2006, Mulchman: 
+//		First created - my birthday!
+
+#include "cbase.h"
+#include "ff_mapfilter.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+//=============================================================================
+//
+// Class CFFMapFilter
+//
+//=============================================================================
+
+//-----------------------------------------------------------------------------
+// Purpose: Constructor
+//-----------------------------------------------------------------------------
+CFFMapFilter::CFFMapFilter( void )
+{
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Deconstructor
+//-----------------------------------------------------------------------------
+CFFMapFilter::~CFFMapFilter( void )
+{
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Used to check if we should reset an entity or not
+//-----------------------------------------------------------------------------
+bool CFFMapFilter::ShouldCreateEntity( const char *pszClassname )
+{
+	if( m_vKeepList.Find( pszClassname ) >= 0 )
+		return false;
+	else
+		return true;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Creates the next entity in our stored list
+//-----------------------------------------------------------------------------
+CBaseEntity *CFFMapFilter::CreateNextEntity( const char *pszClassname )
+{
+	return CreateEntityByName( pszClassname );
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Add an entity to our list
+//-----------------------------------------------------------------------------
+void CFFMapFilter::AddKeep( const char *pszClassname )
+{
+	m_vKeepList.Insert( pszClassname );
+}
