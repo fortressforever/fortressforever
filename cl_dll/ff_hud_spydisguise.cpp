f1cb15a9d4def5862e3671337a6e68cac496d25e
mulchman <mulchman@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2006-09-02 03:22:44 +0000

    spy disguise icon thingie (needs fc's bg&fg magic though)

---
+++ cl_dll/ff_hud_spydisguise.cpp
@@ -0,0 +1,134 @@
+//	=============== Fortress Forever ==============
+//	======== A modification for Half-Life 2 =======
+//
+//	@file ff_hud_spydisguise.cpp
+//	@author Patrick O'Leary (Mulchman)
+//	@date 09/01/2006
+//	@brief Hud Disguise Indicator
+//
+//	REVISIONS
+//	---------
+//	09/01/2006, Mulchman: 
+//		First created
+
+#include "cbase.h"
+#include "hud.h"
+#include "hudelement.h"
+#include "hud_macros.h"
+
+#include <KeyValues.h>
+#include <vgui/ISurface.h>
+#include <vgui/ISystem.h>
+
+#include "c_ff_player.h"
+#include "ff_utils.h"
+#include "c_playerresource.h"
+
+#include <vgui/ILocalize.h>
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+using namespace vgui;
+
+inline void MapClassToGlyph( int iClass, char& cGlyph )
+{
+	/* Straight from the horses mouth
+	!  *	Scout
+	@  *	Sniper
+	#  *	Soldier
+	$  *	Demoman
+	%  *	Medic
+	^  *	HWGuy
+	?  *	Pyro
+	*  *	Spy
+	(  *	Engineer
+	)  *	Civilian
+	_  *	Random
+	*/
+	switch( iClass )
+	{
+		case CLASS_SCOUT: cGlyph = '!'; break;
+		case CLASS_SNIPER: cGlyph = '@'; break;
+		case CLASS_SOLDIER: cGlyph = '#'; break;
+		case CLASS_DEMOMAN: cGlyph = '$'; break;
+		case CLASS_MEDIC: cGlyph = '%'; break;
+		case CLASS_HWGUY: cGlyph = '^'; break;
+		case CLASS_PYRO: cGlyph = '?'; break;
+		case CLASS_SPY: cGlyph = '*'; break;
+		case CLASS_ENGINEER: cGlyph = '('; break;
+		case CLASS_CIVILIAN: cGlyph = ')'; break;
+		default: cGlyph = '_'; break;
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Displays current weapon & ammo
+//-----------------------------------------------------------------------------
+class CHudSpyDisguise : public CHudElement, public vgui::Panel
+{
+public:
+	CHudSpyDisguise( const char *pElementName ) : CHudElement( pElementName ), vgui::Panel( NULL, "HudSpyDisguise" )
+	{
+		// Set our parent window
+		SetParent( g_pClientMode->GetViewport() );
+
+		// Hide when player is dead
+		SetHiddenBits( HIDEHUD_PLAYERDEAD );
+	}
+
+	virtual ~CHudSpyDisguise( void )
+	{
+		if( m_pHudSpyDisguise )
+		{
+			delete m_pHudSpyDisguise;
+			m_pHudSpyDisguise = NULL;
+		}
+	}
+
+	virtual void Paint( void );
+	virtual void VidInit( void );
+
+protected:
+	CHudTexture		*m_pHudSpyDisguise;
+};
+
+DECLARE_HUDELEMENT( CHudSpyDisguise );
+
+void CHudSpyDisguise::VidInit( void )
+{
+	m_pHudSpyDisguise = new CHudTexture;
+	m_pHudSpyDisguise->bRenderUsingFont = true;
+	m_pHudSpyDisguise->hFont = scheme()->GetIScheme( GetScheme() )->GetFont( "ClassGlyphs" );
+	m_pHudSpyDisguise->cCharacterInFont = '_';
+}
+
+void CHudSpyDisguise::Paint( void )
+{
+	if( !engine->IsInGame() )
+		return;
+
+	C_FFPlayer *pPlayer = C_FFPlayer::GetLocalFFPlayer();
+
+	if( !pPlayer )
+		return;
+
+	if( FF_IsPlayerSpec( pPlayer ) || !FF_HasPlayerPickedClass( pPlayer ) )
+		return;
+
+	if( !pPlayer->IsDisguised() )
+		return;
+
+	// Draw!
+	if( m_pHudSpyDisguise )
+	{
+		MapClassToGlyph( pPlayer->GetDisguisedClass(), m_pHudSpyDisguise->cCharacterInFont );
+
+		Color clr = pPlayer->GetTeamColor();
+
+		if( g_PR )
+			clr = g_PR->GetTeamColor( pPlayer->GetDisguisedTeam() );
+
+		m_pHudSpyDisguise->DrawSelf( 0, 0, clr );
+	}
+}
