0e5d571894789b2cecca4d14e2c3b56eabbc6dc3
jiggles <jiggles@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2007-01-22 03:37:15 +0000

    0001374: Made sure no footsteps while cloaked

--- game_shared/ff/ff_player_shared.cpp
+++ game_shared/ff/ff_player_shared.cpp
@@ -435,8 +435,8 @@ void CFFPlayer::PlayStepSound(Vector &vecOrigin, surfacedata_t *psurface, float 
 	m_flIdleTime = gpGlobals->curtime;
 
 	// Don't play footsteps for spy
-	// Jiggles: But do play footsteps while disguised as a non-spy class (Mantis 0001374)
-	if ( GetClassSlot() != 8 || ( IsDisguised() && GetDisguisedClass() != 8 ) )
+	// Jiggles: 0001374: But do play footsteps while disguised as a non-spy class, unless cloaked 
+	if ( GetClassSlot() != 8 || ( !IsCloaked() && IsDisguised() && GetDisguisedClass() != 8 ) )
 		BaseClass::PlayStepSound(vecOrigin, psurface, fvol, force);
 }
 // <-- Mirv: Proper sounds
