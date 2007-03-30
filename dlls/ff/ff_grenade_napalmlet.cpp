6854a1942763b22600da13e5cbe2d24825035947
jiggles <jiggles@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2007-03-30 18:31:04 +0000

    Changed Napalm Grenade Functionality
    Still to do:
    Add check to remove napalm-let when it hits water
    Find better gib model (currently using the same skull gib as the IC)
    Other stuff :)

---
+++ dlls/ff/ff_grenade_napalmlet.cpp
@@ -0,0 +1,291 @@
+
+
+#include "cbase.h"
+#include "ff_grenade_base.h"
+
+#include "ff_grenade_napalmlet.h"
+#include "ff_utils.h"
+
+#ifndef CLIENT_DLL
+	#include "ff_player.h"
+#endif
+
+BEGIN_DATADESC( CFFGrenadeNapalmlet )
+	DEFINE_THINKFUNC( FlameThink ),
+END_DATADESC()
+
+
+LINK_ENTITY_TO_CLASS( ff_grenade_napalmlet, CFFGrenadeNapalmlet );
+PRECACHE_WEAPON_REGISTER( ff_grenade_napalmlet );
+
+
+void CFFGrenadeNapalmlet::UpdateOnRemove( void )
+{
+	StopSound( "General.BurningFlesh" );
+	StopSound( "General.BurningObject" );
+	//EmitSound( "General.StopBurning" );
+
+	BaseClass::UpdateOnRemove();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Constructor
+//-----------------------------------------------------------------------------
+//CFFGrenadeNapalmlet::CFFGrenadeNapalmlet( void ){}
+
+//-----------------------------------------------------------------------------
+// Purpose: Precache assets
+//-----------------------------------------------------------------------------
+void CFFGrenadeNapalmlet::Precache ( void )
+{
+	PrecacheModel( NAPALMLET_MODEL );
+	BaseClass::Precache();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Spawn
+//-----------------------------------------------------------------------------
+void CFFGrenadeNapalmlet::Spawn( void )
+{
+	//m_flSpawnTime = gpGlobals->curtime;
+	BaseClass::Spawn();
+
+	SetModel ( NAPALMLET_MODEL );
+	SetAbsAngles( QAngle( 0, 0, 0 ) );
+	SetSolidFlags( FSOLID_TRIGGER | FSOLID_NOT_STANDABLE );
+	SetMoveType( MOVETYPE_FLYGRAVITY, MOVECOLLIDE_FLY_CUSTOM );
+	SetSolid( SOLID_BBOX );
+	SetCollisionGroup( COLLISION_GROUP_DEBRIS );
+	SetSize( Vector ( -5, -5, -5 ), Vector ( 5, 5, 5 ) );
+	SetThink( &CFFGrenadeNapalmlet::FlameThink );
+	SetNextThink( gpGlobals->curtime );
+	SetEffects(EF_NOSHADOW);
+
+	m_pFlame = CEntityFlame::Create( this, false );
+	if (m_pFlame)
+	{
+		m_pFlame->SetLifetime( m_flBurnTime );
+		AddFlag( FL_ONFIRE );
+		SetEffectEntity( m_pFlame );
+		m_pFlame->SetSize( 50.0f );
+	}
+}
+
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CFFGrenadeNapalmlet::ResolveFlyCollisionCustom( trace_t &trace, Vector &vecVelocity )
+{
+	//Assume all surfaces have the same elasticity
+	float flSurfaceElasticity = 1.0;
+
+	//Don't bounce off of players with perfect elasticity
+	if( trace.m_pEnt && trace.m_pEnt->IsPlayer() )
+	{
+		flSurfaceElasticity = 0.3;
+	}
+
+	// if its breakable glass and we kill it, don't bounce.
+	// give some damage to the glass, and if it breaks, pass 
+	// through it.
+	bool breakthrough = false;
+
+	if( trace.m_pEnt && FClassnameIs( trace.m_pEnt, "func_breakable" ) )
+	{
+		breakthrough = true;
+	}
+
+	if( trace.m_pEnt && FClassnameIs( trace.m_pEnt, "func_breakable_surf" ) )
+	{
+		breakthrough = true;
+	}
+
+	if (breakthrough)
+	{
+		CTakeDamageInfo info( this, this, 10, DMG_CLUB );
+		trace.m_pEnt->DispatchTraceAttack( info, GetAbsVelocity(), &trace );
+
+		ApplyMultiDamage();
+
+		if( trace.m_pEnt->m_iHealth <= 0 )
+		{
+			// slow our flight a little bit
+			Vector vel = GetAbsVelocity();
+
+			vel *= 0.4;
+
+			SetAbsVelocity( vel );
+			return;
+		}
+	}
+
+	float flTotalElasticity = GetElasticity() * flSurfaceElasticity;
+	flTotalElasticity = clamp( flTotalElasticity, 0.0f, 0.9f );
+
+	// NOTE: A backoff of 2.0f is a reflection
+	Vector vecAbsVelocity;
+	PhysicsClipVelocity( GetAbsVelocity(), trace.plane.normal, vecAbsVelocity, 2.0f );
+	vecAbsVelocity *= flTotalElasticity;
+
+	// Get the total velocity (player + conveyors, etc.)
+	VectorAdd( vecAbsVelocity, GetBaseVelocity(), vecVelocity );
+	float flSpeedSqr = DotProduct( vecVelocity, vecVelocity );
+
+	// Stop if on ground.
+	if ( trace.plane.normal.z > 0.7f )			// Floor
+	{
+		// Verify that we have an entity.
+		CBaseEntity *pEntity = trace.m_pEnt;
+		Assert( pEntity );
+
+		SetAbsVelocity( vecAbsVelocity );
+
+		if ( flSpeedSqr < ( 30 * 30 ) )
+		{
+			if ( pEntity->IsStandable() )
+			{
+				SetGroundEntity( pEntity );
+			}
+
+			// Reset velocities.
+			SetAbsVelocity( vec3_origin );
+			SetLocalAngularVelocity( vec3_angle );
+
+			/*//align to the ground so we're not standing on end
+			QAngle angle;
+			VectorAngles( trace.plane.normal, angle );
+
+			// rotate randomly in yaw
+			angle[1] = random->RandomFloat( 0, 360 );
+
+			// TODO: rotate around trace.plane.normal
+
+			SetAbsAngles( angle );	*/		
+		}
+		else
+		{
+			Vector vecDelta = GetBaseVelocity() - vecAbsVelocity;	
+			Vector vecBaseDir = GetBaseVelocity();
+			VectorNormalize( vecBaseDir );
+			float flScale = vecDelta.Dot( vecBaseDir );
+
+			VectorScale( vecAbsVelocity, ( 1.0f - trace.fraction ) * gpGlobals->frametime, vecVelocity ); 
+			VectorMA( vecVelocity, ( 1.0f - trace.fraction ) * gpGlobals->frametime, GetBaseVelocity() * flScale, vecVelocity );
+			PhysicsPushEntity( vecVelocity, &trace );
+		}
+	}
+	else
+	{
+		// If we get *too* slow, we'll stick without ever coming to rest because
+		// we'll get pushed down by gravity faster than we can escape from the wall.
+		if ( flSpeedSqr < ( 30 * 30 ) )
+		{
+			// Reset velocities.
+			SetAbsVelocity( vec3_origin );
+			SetLocalAngularVelocity( vec3_angle );
+		}
+		else
+		{
+			SetAbsVelocity( vecAbsVelocity );
+		}
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Burninate the players
+//-----------------------------------------------------------------------------
+void CFFGrenadeNapalmlet::FlameThink()
+	{
+		// Remove if we've reached the end of our fuse
+		if( gpGlobals->curtime > m_flBurnTime )
+		{
+			UTIL_Remove(this);
+			return;
+		}
+		// Jiggles: We stopped moving; let's switch to a standing flame
+		// Nevermind, this doesn't work.
+		//if( m_bFlameSwitch && GetAbsVelocity() == vec3_origin )
+		//{
+		//	if (m_pFlame)
+		//		m_pFlame->SetUseHitboxes(true);
+		//	m_bFlameSwitch = false;
+		//}
+
+
+		//SetAbsVelocity( Vector( 0, 0, 10 + 20 * sin( DEG2RAD( GetAbsAngles().y ) ) ) );
+		//SetAbsAngles( GetAbsAngles() + QAngle( 0, 15, 0 ) );
+
+		//Vector vecForward;
+		//AngleVectors( GetAbsAngles(), &vecForward );
+
+		if((gpGlobals->curtime - m_flLastBurnCheck) >= 1.0f)
+		{
+			m_flLastBurnCheck = gpGlobals->curtime;
+
+			Vector	vecSrc = GetAbsOrigin();
+			vecSrc.z += 1;
+
+			CBaseEntity *pEntity = NULL;
+
+			for( CEntitySphereQuery sphere( vecSrc, m_flBurnRadius ); ( pEntity = sphere.GetCurrentEntity() ) != NULL; sphere.NextEntity() )
+			{
+				if( !pEntity )
+					continue;
+
+				// Bug #0000269: Napalm through walls.
+				// Mulch: if we hit water w/ the trace, abort too!
+				trace_t tr;
+				UTIL_TraceLine(GetAbsOrigin(), pEntity->GetAbsOrigin(), MASK_SOLID_BRUSHONLY | CONTENTS_WATER, this, COLLISION_GROUP_DEBRIS, &tr);
+
+				if (tr.fraction < 1.0f)
+					continue;
+
+				// Bug #0000270: Napalm grenade burn radius reaches unrealisticly high.
+				float height = tr.startpos.z - tr.endpos.z;
+				if (height < -40.0f || height > 40.0f)
+					continue;
+
+				// Don't damage if entity is more than feet deep in water
+				if( pEntity->GetWaterLevel() >= 2 )
+					continue;
+
+				switch( pEntity->Classify() )
+				{
+					case CLASS_PLAYER:
+					{
+						CFFPlayer *pPlayer = ToFFPlayer( pEntity );
+						if( !pPlayer )
+							continue;
+
+						if (g_pGameRules->FPlayerCanTakeDamage(pPlayer, GetOwnerEntity()))
+						{
+							pPlayer->TakeDamage( CTakeDamageInfo( this, GetOwnerEntity(), m_flBurnDamage, DMG_BURN ) );
+							pPlayer->ApplyBurning( ToFFPlayer( GetOwnerEntity() ), 5.0f, 10.0f, BURNTYPE_NALPALMGRENADE);
+						}
+					}
+					break;
+					case CLASS_SENTRYGUN:
+					case CLASS_DISPENSER:
+					{
+						CFFBuildableObject *pBuildable = dynamic_cast< CFFBuildableObject * >( pEntity );
+						if( !pBuildable )
+							continue;
+						
+						CFFPlayer *pPlayer = pBuildable->GetOwnerPlayer();
+						if( !pPlayer )
+							continue;
+
+						if (g_pGameRules->FPlayerCanTakeDamage(pPlayer, GetOwnerEntity()))
+							pBuildable->TakeDamage( CTakeDamageInfo( this, GetOwnerEntity(), 8.0f, DMG_BURN ) );
+					}
+					
+					default:
+						break;
+				}
+			}
+		}
+
+		SetNextThink( gpGlobals->curtime + 1.0f );
+	}
+
--- game_shared/ff/ff_grenade_napalm.cpp
+++ game_shared/ff/ff_grenade_napalm.cpp
@@ -20,18 +20,25 @@
 	#include "baseentity.h"
 	#include "ff_entity_system.h"
 	#include "te_effect_dispatch.h"
+
+	extern short g_sModelIndexFireball;
+	extern short g_sModelIndexWExplosion;
 #endif
 
 #define NAPALMGRENADE_MODEL		"models/grenades/napalm/napalm.mdl"
-#define NAPALM_EFFECT			"NapalmBurst"
+//#define NAPALM_EFFECT			"NapalmBurst"
 
 #ifdef CLIENT_DLL
 	#define CFFGrenadeNapalm C_FFGrenadeNapalm
-#endif
-
-#ifndef CLIENT_DLL
+	#include "clienteffectprecachesystem.h"
+	#include "particles_simple.h"
+#else
 	ConVar nap_flame_time("ffdev_nap_flame_time","5.0",0,"How long the napalm grenade's fires burn");
-	ConVar nap_burn_damage("ffdev_nap_burn_damage","1.0",0,"How much damage being in the radius of a napalm grenade deals.");
+	ConVar nap_burn_damage("ffdev_nap_burn_damage","15.0",0,"How much damage being in the radius of a napalm grenade deals.");
+	ConVar nap_burn_radius("ffdev_nap_burn_radius","98.0",0,"Burn radius of a napalmlet.");
+	#include "EntityFlame.h"
+	#include "smoke_trail.h"
+	#include "ff_grenade_napalmlet.h"
 #endif
 
 class CFFGrenadeNapalm : public CFFGrenadeBase
@@ -47,23 +54,26 @@ public:
 
 	float m_flLastBurnCheck;
 
+
 #ifdef CLIENT_DLL
 	CFFGrenadeNapalm() {}
 	CFFGrenadeNapalm( const CFFGrenadeNapalm& ) {}
 
 	virtual RenderGroup_t GetRenderGroup()  { return RENDER_GROUP_TWOPASS; }
+	static PMaterialHandle m_hHeatwaveMaterial;
 
 #else
 	DECLARE_DATADESC()
 	virtual void Spawn();
 	virtual void Explode( trace_t *pTrace, int bitsDamageType );
-	void FlameThink(void);
+	void SmokeThink(void);
+	SmokeTrail *m_pSmokeTrail;
 #endif
 };
 
 #ifdef GAME_DLL
 	BEGIN_DATADESC(CFFGrenadeNapalm)
-		DEFINE_THINKFUNC( FlameThink ),
+		DEFINE_THINKFUNC( SmokeThink ),
 	END_DATADESC()
 #endif
 
@@ -74,6 +84,8 @@ END_NETWORK_TABLE()
 
 LINK_ENTITY_TO_CLASS( ff_grenade_napalm, CFFGrenadeNapalm );
 PRECACHE_WEAPON_REGISTER( ff_grenade_napalm );
+
+
 
 void CFFGrenadeNapalm::UpdateOnRemove( void )
 {
@@ -92,6 +104,7 @@ void CFFGrenadeNapalm::UpdateOnRemove( void )
 	void CFFGrenadeNapalm::Spawn()
 	{
 		SetModel(NAPALMGRENADE_MODEL);
+		m_pSmokeTrail=NULL;
 
 		BaseClass::Spawn();
 	}
@@ -109,14 +122,158 @@ void CFFGrenadeNapalm::UpdateOnRemove( void )
 			return;
 		}
 		
+		//BaseClass::Explode( pTrace, bitsDamageType );
+		
+		// Jiggles: This is a paste of BaseClass::Explode() code, modified to remove the standard grenade explosion
+		//			 and damage / blastforce
+		SetModelName( NULL_STRING );//invisible
+		AddSolidFlags( FSOLID_NOT_SOLID );
+
+		m_takedamage = DAMAGE_NO;
+
+		// Make sure grenade explosion is 32.0f above the ground.
+		// In TFC exploding grenade explosions are ALWAYS 32.0f above the floor, except
+		// in the case of the concussion grenade.
+		if( pTrace->fraction != 1.0 )
+			SetLocalOrigin( pTrace->endpos + ( pTrace->plane.normal * 32 ) );
+
+		if( FFScriptRunPredicates( this, "onexplode", true ) )
+		{
+			Vector vecAbsOrigin = GetAbsOrigin();
+			int contents = UTIL_PointContents( vecAbsOrigin );
+
+			if( pTrace->fraction != 1.0 ) 
+			{
+				Vector vecNormal = pTrace->plane.normal;
+				surfacedata_t *pdata = physprops->GetSurfaceData( pTrace->surface.surfaceProps );	
+				CPASFilter filter( vecAbsOrigin );
+				te->Explosion( filter, -1.0, // don't apply cl_interp delay
+					&vecAbsOrigin, 
+					! ( contents & MASK_WATER ) ? g_sModelIndexFireball : g_sModelIndexWExplosion, 
+					m_flDamage / 160, 
+					25, 
+					TE_EXPLFLAG_NONE, 
+					m_DmgRadius, 
+					m_flDamage, 
+					&vecNormal, 
+					( char )pdata->game.material );
+
+				// Normal decals since trace hit something
+				UTIL_DecalTrace( pTrace, "Scorch" );
+			}
+			else
+			{
+				CPASFilter filter( vecAbsOrigin );
+				te->Explosion( filter, -1.0, // don't apply cl_interp delay
+					&vecAbsOrigin, 
+					! ( contents & MASK_WATER ) ? g_sModelIndexFireball : g_sModelIndexWExplosion, 
+					m_flDamage / 160, 
+					25, 
+					TE_EXPLFLAG_NONE, 
+					m_DmgRadius, 
+					m_flDamage );
+
+				// Trace hit nothing so do custom scorch mark finding
+				FF_DecalTrace( this, FF_DECALTRACE_TRACE_DIST, "Scorch" );
+			}
+
+			//CSoundEnt::InsertSound( SOUND_COMBAT, GetAbsOrigin(), BASEGRENADE_EXPLOSION_VOLUME, 3.0 );
+
+			CBaseEntity *pThrower = GetThrower();
+			// Use the grenade's position as the reported position
+			Vector vecReported = pTrace->endpos;
+			CTakeDamageInfo info( this, pThrower, GetBlastForce()/4, GetAbsOrigin(), 0.0f/*m_flDamage*/, bitsDamageType, 0, &vecReported );
+			RadiusDamage( info, GetAbsOrigin(), m_DmgRadius, CLASS_NONE, NULL );
+
+			//EmitSound( "BaseGrenade.Explode" );
+		}
+
+		SetThink( SmokeThink );
+		SetTouch( NULL );
+
+		AddEffects( EF_NODRAW );
+		//SetAbsVelocity( vec3_origin );
+	
+		SetNextThink( gpGlobals->curtime );
+		//	Jiggles: End paste
+
+		CBaseEntity *pOwner = GetOwnerEntity();
+
+		if(!pOwner)
+		{
+			Warning("BIG PROBLEM: NULL OWNER (%s)\n", __FUNCTION__);
+			return;
+		}
+
 		// Bug #0000370: napalm explosion not playing
 		EmitSound("Napalm.Explode");
 
-		CEffectData data;
-		data.m_vOrigin = GetAbsOrigin();
-		data.m_flScale = 1.0f;
 
-		DispatchEffect(NAPALM_EFFECT, data);
+		for ( int i = 0; i < 8; i++ )
+		{
+			Vector vecSrc = GetAbsOrigin();
+			QAngle angSpawn;
+
+			angSpawn.x = RandomFloat(45.0f,75.0f);
+			angSpawn.y = RandomFloat(0.0f, 360.0f);
+			angSpawn.z = 0.0f;
+
+			Vector vecVelocity;
+			AngleVectors(angSpawn,&vecVelocity);
+			vecVelocity *= RandomFloat(250.0f,350.0f);
+
+			// So they don't begin moving down, I guess
+			if (vecVelocity.z < 0)
+				vecVelocity.z *= -1;
+
+			CFFGrenadeNapalmlet *pNaplet = (CFFGrenadeNapalmlet *)CreateEntityByName( "ff_grenade_napalmlet" );
+
+			if (!pNaplet)
+				continue;
+
+			// Jiggles: Instead, each Naplet makes it's own flame
+			//CEntityFlame *pFlame = CEntityFlame::Create( pNaplet, false );
+			//if (pFlame)
+			//{
+			//	pFlame->SetLifetime( nap_flame_time.GetFloat() );
+			//	AddFlag( FL_ONFIRE );
+			//	SetEffectEntity( pFlame );
+			//	pFlame->SetSize( 60.0f );
+			//}
+
+			
+
+			QAngle angRotate;
+			angRotate.x = RandomFloat(-360.0f, 360.0f);
+			angRotate.y = RandomFloat(-360.0f, 360.0f);
+			angRotate.z = 2.0*RandomFloat(-360.0f, 360.0f);
+
+			UTIL_SetOrigin( pNaplet, vecSrc );
+			pNaplet->SetAbsAngles(QAngle(0,0,0)); //make the model stand on end
+			pNaplet->SetLocalAngularVelocity(angRotate);
+			pNaplet->Spawn();
+
+			pNaplet->SetBurnRadius( nap_burn_radius.GetFloat() );
+			pNaplet->SetBurnTime( nap_flame_time.GetFloat() );
+			pNaplet->SetBurnDamage( nap_burn_damage.GetFloat() );
+			pNaplet->SetOwnerEntity( pOwner );
+
+
+			pNaplet->SetAbsVelocity( vecVelocity );
+			//pNaplet->SetupInitialTransmittedVelocity( vecVelocity );
+			pNaplet->SetElasticity( 0.2f );
+
+			pNaplet->ChangeTeam( pOwner->GetTeamNumber() );
+			pNaplet->SetGravity( GetGrenadeGravity() + 0.1f );
+			pNaplet->SetFriction( GetGrenadeFriction() );
+		}
+
+
+		//CEffectData data;
+		//data.m_vOrigin = GetAbsOrigin();
+		//data.m_flScale = 1.0f;
+
+		//DispatchEffect(NAPALM_EFFECT, data);
 
 		// Now do a napalm burning think
 		SetDetonateTimerLength(nap_flame_time.GetFloat());
@@ -124,15 +281,17 @@ void CFFGrenadeNapalm::UpdateOnRemove( void )
 		// Should this maybe be noclip?
 		SetMoveType( MOVETYPE_NONE );
 
-		m_flLastBurnCheck = 0;
-		SetThink(&CFFGrenadeNapalm::FlameThink);
+		//m_flLastBurnCheck = 0;
+		SetThink(&CFFGrenadeNapalm::SmokeThink);
 		SetNextThink(gpGlobals->curtime);
 	}
 
 	//-----------------------------------------------------------------------------
-	// Purpose: Hurt nearby people and things
+	// Purpose: Generate Smoke
+	//			Jiggles: This used to be FlameThink, but I changed it so that each
+	//					 Naplet does its own FlameThink
 	//-----------------------------------------------------------------------------
-	void CFFGrenadeNapalm::FlameThink()
+	void CFFGrenadeNapalm::SmokeThink()
 	{
 		// Remove if we've reached the end of our fuse
 		if( gpGlobals->curtime > m_flDetonateTime )
@@ -141,82 +300,107 @@ void CFFGrenadeNapalm::UpdateOnRemove( void )
 			return;
 		}
 
-		//SetAbsVelocity( Vector( 0, 0, 10 + 20 * sin( DEG2RAD( GetAbsAngles().y ) ) ) );
-		//SetAbsAngles( GetAbsAngles() + QAngle( 0, 15, 0 ) );
-
-		//Vector vecForward;
-		//AngleVectors( GetAbsAngles(), &vecForward );
-
-		if((gpGlobals->curtime - m_flLastBurnCheck) >= 1.0f)
+		if (!m_pSmokeTrail)
 		{
-			m_flLastBurnCheck = gpGlobals->curtime;
-
-			float	flRadius = GetGrenadeRadius();
-			Vector	vecSrc = GetAbsOrigin();
-			vecSrc.z += 1;
-
-			CBaseEntity *pEntity = NULL;
-
-			for( CEntitySphereQuery sphere( vecSrc, flRadius ); ( pEntity = sphere.GetCurrentEntity() ) != NULL; sphere.NextEntity() )
+			m_pSmokeTrail =  SmokeTrail::CreateSmokeTrail();
+			if (m_pSmokeTrail)
 			{
-				if( !pEntity )
-					continue;
-
-				// Bug #0000269: Napalm through walls.
-				// Mulch: if we hit water w/ the trace, abort too!
-				trace_t tr;
-				UTIL_TraceLine(GetAbsOrigin(), pEntity->GetAbsOrigin(), MASK_SOLID_BRUSHONLY | CONTENTS_WATER, this, COLLISION_GROUP_DEBRIS, &tr);
-
-				if (tr.fraction < 1.0f)
-					continue;
-
-				// Bug #0000270: Napalm grenade burn radius reaches unrealisticly high.
-				float height = tr.startpos.z - tr.endpos.z;
-				if (height < -40.0f || height > 40.0f)
-					continue;
-
-				// Don't damage if entity is more than feet deep in water
-				if( pEntity->GetWaterLevel() >= 2 )
-					continue;
-
-				switch( pEntity->Classify() )
-				{
-					case CLASS_PLAYER:
-					{
-						CFFPlayer *pPlayer = ToFFPlayer( pEntity );
-						if( !pPlayer )
-							continue;
-
-						if (g_pGameRules->FPlayerCanTakeDamage(pPlayer, GetOwnerEntity()))
-						{
-							pPlayer->TakeDamage( CTakeDamageInfo( this, GetOwnerEntity(), 10.0f, DMG_BURN ) );
-							pPlayer->ApplyBurning( ToFFPlayer( GetOwnerEntity() ), 5.0f, 10.0f, BURNTYPE_NALPALMGRENADE);
-						}
-					}
-					break;
-					case CLASS_SENTRYGUN:
-					case CLASS_DISPENSER:
-					{
-						CFFBuildableObject *pBuildable = dynamic_cast< CFFBuildableObject * >( pEntity );
-						if( !pBuildable )
-							continue;
-						
-						CFFPlayer *pPlayer = pBuildable->GetOwnerPlayer();
-						if( !pPlayer )
-							continue;
-
-						if (g_pGameRules->FPlayerCanTakeDamage(pPlayer, GetOwnerEntity()))
-							pBuildable->TakeDamage( CTakeDamageInfo( this, GetOwnerEntity(), 8.0f, DMG_BURN ) );
-					}
-					
-					default:
-						break;
-				}
+				m_pSmokeTrail->FollowEntity( this );
+				m_pSmokeTrail->m_SpawnRate = 4;
+				m_pSmokeTrail->m_ParticleLifetime = 3.0f;
+				m_pSmokeTrail->m_StartColor.Init( 0.7f, 0.7f, 0.7f );
+				m_pSmokeTrail->m_EndColor.Init( 0.6, 0.6, 0.6 );
+				m_pSmokeTrail->m_StartSize = 86;
+				m_pSmokeTrail->m_EndSize = 196;
+				m_pSmokeTrail->m_SpawnRadius = 4;
+				m_pSmokeTrail->m_Opacity = 0.5f;
+				m_pSmokeTrail->m_MinSpeed = 16;
+				m_pSmokeTrail->m_MaxSpeed = 16;
+				m_pSmokeTrail->m_MinDirectedSpeed	= 16.0f;
+				m_pSmokeTrail->m_MaxDirectedSpeed	= 16.0f;
+				m_pSmokeTrail->SetLifetime( 5.0f );
 			}
 		}
-
-		SetNextThink( gpGlobals->curtime );
+		SetNextThink( m_flDetonateTime );
 	}
+// Jiggles: Old burn code below
+//
+//		//SetAbsVelocity( Vector( 0, 0, 10 + 20 * sin( DEG2RAD( GetAbsAngles().y ) ) ) );
+//		//SetAbsAngles( GetAbsAngles() + QAngle( 0, 15, 0 ) );
+//
+//		//Vector vecForward;
+//		//AngleVectors( GetAbsAngles(), &vecForward );
+//
+//		if((gpGlobals->curtime - m_flLastBurnCheck) >= 1.0f)
+//		{
+//			m_flLastBurnCheck = gpGlobals->curtime;
+//
+//			float	flRadius = GetGrenadeRadius();
+//			Vector	vecSrc = GetAbsOrigin();
+//			vecSrc.z += 1;
+//
+//			CBaseEntity *pEntity = NULL;
+//
+//			for( CEntitySphereQuery sphere( vecSrc, flRadius ); ( pEntity = sphere.GetCurrentEntity() ) != NULL; sphere.NextEntity() )
+//			{
+//				if( !pEntity )
+//					continue;
+//
+//				// Bug #0000269: Napalm through walls.
+//				// Mulch: if we hit water w/ the trace, abort too!
+//				trace_t tr;
+//				UTIL_TraceLine(GetAbsOrigin(), pEntity->GetAbsOrigin(), MASK_SOLID_BRUSHONLY | CONTENTS_WATER, this, COLLISION_GROUP_DEBRIS, &tr);
+//
+//				if (tr.fraction < 1.0f)
+//					continue;
+//
+//				// Bug #0000270: Napalm grenade burn radius reaches unrealisticly high.
+//				float height = tr.startpos.z - tr.endpos.z;
+//				if (height < -40.0f || height > 40.0f)
+//					continue;
+//
+//				// Don't damage if entity is more than feet deep in water
+//				if( pEntity->GetWaterLevel() >= 2 )
+//					continue;
+//
+//				switch( pEntity->Classify() )
+//				{
+//					case CLASS_PLAYER:
+//					{
+//						CFFPlayer *pPlayer = ToFFPlayer( pEntity );
+//						if( !pPlayer )
+//							continue;
+//
+//						if (g_pGameRules->FPlayerCanTakeDamage(pPlayer, GetOwnerEntity()))
+//						{
+//							pPlayer->TakeDamage( CTakeDamageInfo( this, GetOwnerEntity(), 10.0f, DMG_BURN ) );
+//							pPlayer->ApplyBurning( ToFFPlayer( GetOwnerEntity() ), 5.0f, 10.0f, BURNTYPE_NALPALMGRENADE);
+//						}
+//					}
+//					break;
+//					case CLASS_SENTRYGUN:
+//					case CLASS_DISPENSER:
+//					{
+//						CFFBuildableObject *pBuildable = dynamic_cast< CFFBuildableObject * >( pEntity );
+//						if( !pBuildable )
+//							continue;
+//						
+//						CFFPlayer *pPlayer = pBuildable->GetOwnerPlayer();
+//						if( !pPlayer )
+//							continue;
+//
+//						if (g_pGameRules->FPlayerCanTakeDamage(pPlayer, GetOwnerEntity()))
+//							pBuildable->TakeDamage( CTakeDamageInfo( this, GetOwnerEntity(), 8.0f, DMG_BURN ) );
+//					}
+//					
+//					default:
+//						break;
+//				}
+//			}
+//		}
+//
+//		SetNextThink( gpGlobals->curtime );
+//	}
 #endif
 
 //-----------------------------------------------------------------------------
