644c654c18355355d3bbe16f2b92eb68c2827758
mulchman <mulchman@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2007-05-02 16:53:50 +0000

    0001520: Sniper has footsteps while charging sniper rifle

--- game_shared/ff/ff_weapon_sniperrifle.cpp
+++ game_shared/ff/ff_weapon_sniperrifle.cpp
@@ -10,43 +10,10 @@
 /// ---------
 /// Jan 24, 2005 Mirv: First implementation
 
-
 #include "cbase.h"
-#include "ff_weapon_base.h"
-#include "ff_fx_shared.h"
-#include "in_buttons.h"
-#include "Sprite.h"
-
-#ifdef CLIENT_DLL
-	#include "fx.h"
-	#include "fx_sparks.h"
-	#include "fx_line.h"
-	#include "model_types.h"
-	#include "ff_utils.h"
-	#include "c_ff_player.h"
-#endif
-
-//#define	RPG_LASER_SPRITE	"sprites/redglow1"
+#include "ff_weapon_sniperrifle.h"
 
 static int g_iBeam, g_iHalo;
-
-#define SNIPER_BEAM		"effects/bluelaser1.vmt"
-#define SNIPER_HALO		"sprites/muzzleflash1.vmt"
-#define SNIPER_DOT		"sprites/sniperdot_red.vmt"
-
-
-#ifdef CLIENT_DLL 
-	#define CFFWeaponSniperRifle C_FFWeaponSniperRifle
-	#define CFFWeaponLaserDot C_FFWeaponLaserDot
-	#include "c_ff_player.h"
-	#include "view.h"
-
-	#include "iviewrender_beams.h"
-	#include "beam_shared.h"
-	#include "beamdraw.h"
-#else
-	#include "ff_player.h"
-#endif
 
 #ifdef CLIENT_DLL
 	ConVar laser_beam_angle("ffdev_laserbeamangle", "0.01");
@@ -58,48 +25,6 @@ ConVar sniperrifle_zoomfov("ffdev_sniperrifle_zoomfov", "25", FCVAR_REPLICATED, 
 //=============================================================================
 // CFFWeaponLaserDot
 //=============================================================================
-class CFFWeaponLaserDot : public CSprite
-{
-public:
-	DECLARE_CLASS(CFFWeaponLaserDot, CSprite);
-
-	DECLARE_NETWORKCLASS();
-	DECLARE_DATADESC();
-
-#ifdef CLIENT_DLL
-	CFFWeaponLaserDot();
-	virtual void GetRenderBounds(Vector& mins, Vector& maxs);
-#endif
-
-	static	CFFWeaponLaserDot *Create(const Vector &origin, CBaseEntity *pOwner = NULL);
-
-	void	SetLaserPosition(const Vector &origin);
-
-	bool	IsOn() const		{ return m_bIsOn; }
-
-	void	TurnOn() 		{ m_bIsOn = true; }
-	void	TurnOff() 		{ m_bIsOn = false; }
-	void	Toggle() 		{ m_bIsOn = !m_bIsOn; }
-
-	int		ObjectCaps() { return (BaseClass::ObjectCaps() & ~FCAP_ACROSS_TRANSITION) | FCAP_DONT_SAVE; }
-
-#ifdef CLIENT_DLL
-
-	virtual bool			IsTransparent() { return true; }
-	virtual RenderGroup_t	GetRenderGroup() { return RENDER_GROUP_TRANSLUCENT_ENTITY; }
-	virtual int				DrawModel(int flags);
-	virtual void			OnDataChanged(DataUpdateType_t updateType);
-	virtual bool			ShouldDraw() { return (IsEffectActive(EF_NODRAW) ==false); }
-
-	IMaterial	*m_pMaterial;
-#endif
-
-	CNetworkVar(float, m_flStartTime);
-
-protected:
-	bool				m_bIsOn;
-
-};
 
 //=============================================================================
 // CFFWeaponLaserDot tables
@@ -367,64 +292,6 @@ void CFFWeaponLaserDot::SetLaserPosition(const Vector &origin)
 //=============================================================================
 // CFFWeaponSniperRifle
 //=============================================================================
-
-class CFFWeaponSniperRifle : public CFFWeaponBase
-{
-public:
-	DECLARE_CLASS(CFFWeaponSniperRifle, CFFWeaponBase);
-	DECLARE_NETWORKCLASS(); 
-	DECLARE_PREDICTABLE();
-
-	CFFWeaponSniperRifle();
-	~CFFWeaponSniperRifle();
-
-	virtual void Precache();
-	virtual void PrimaryAttack();
-	virtual void SecondaryAttack();
-	virtual void Spawn();
-	virtual void Fire();
-
-	void ToggleZoom();
-
-	virtual bool Deploy();
-	virtual bool Holster(CBaseCombatWeapon *pSwitchingTo);
-
-	virtual void ItemPostFrame();
-	virtual void ItemBusyFrame();
-
-#ifdef CLIENT_DLL
-	virtual float GetFOV();
-#endif
-
-	void UpdateLaserPosition();
-
-	virtual FFWeaponID GetWeaponID() const		{ return FF_WEAPON_SNIPERRIFLE; }
-
-	virtual float GetRecoilMultiplier( void );
-
-	float GetFireStartTime( void ) const		{ return m_flFireStartTime; }
-	bool IsInFire( void ) const					{ return m_bInFire; }
-
-private:
-	bool m_bZoomed;
-	bool m_bInFire;
-
-	CFFWeaponSniperRifle(const CFFWeaponSniperRifle &);
-
-	void CheckZoomToggle();
-	void CheckFire();
-
-#ifdef GAME_DLL
-	CHandle<CFFWeaponLaserDot>	m_hLaserDot;
-#endif
-
-	float m_flFireStartTime;
-
-#ifdef CLIENT_DLL
-	float m_flZoomTime;
-	float m_flNextZoomTime;
-#endif
-};
 
 //=============================================================================
 // CFFWeaponSniperRifle tables
@@ -872,8 +739,13 @@ float CFFWeaponSniperRifle::GetRecoilMultiplier()
 
 #ifdef CLIENT_DLL
 float GetSniperRifleCharge( void )
+#else
+float GetSniperRifleCharge( CFFPlayer *pPlayer )
+#endif
 {
+#ifdef CLIENT_DLL
 	CFFPlayer *pPlayer = CFFPlayer::GetLocalFFPlayer();
+#endif
 	if( !pPlayer )
 		return 0.0f;
 
@@ -890,4 +762,3 @@ float GetSniperRifleCharge( void )
 
 	return 100.0f * ( clamp( gpGlobals->curtime - pSniperRifle->GetFireStartTime(), 1.0f, 7.0f ) / 7.0f );
 }
-#endif
