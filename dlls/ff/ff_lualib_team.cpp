46afd5a6a23d36414e970a7f57d46e053d962e08
TrepidJon <jday@trepid.net>
2007-09-23 02:22:40 +0000

    Added more lua shitdicks.

--- dlls/ff/ff_lualib_team.cpp
+++ dlls/ff/ff_lualib_team.cpp
@@ -35,9 +35,19 @@ void CFFLuaLib::InitTeam(lua_State* L)
 		// CTeam
 		class_<CTeam>("BaseTeam")
 			.def("AddScore",			&CTeam::AddScore)
+			.def("GetScore",			&CTeam::GetScore)
+			.def("SetScore",			&CTeam::SetScore)
+			.def("GetScoreTime",		&CTeam::GetScoreTime)
+			.def("AddFortPoints",		&CTeam::AddFortPoints)
+			.def("SetFortPoints",		&CTeam::SetFortPoints)
+			.def("GetFortPoints",		&CTeam::GetFortPoints)
+			.def("AddDeaths",			&CTeam::AddDeaths)
+			.def("GetDeaths",			&CTeam::GetDeaths)
+			.def("SetDeaths",			&CTeam::SetDeaths)
 			.def("GetNumPlayers",		&CTeam::GetNumPlayers)
 			.def("GetPlayer",			&CTeam::GetPlayer)
 			.def("GetTeamId",			&CTeam::GetTeamNumber)
+			.def("GetName",				&CTeam::GetName)
 			.def("SetName",				&CTeam::SetName),
 
 		// CFFTeam
