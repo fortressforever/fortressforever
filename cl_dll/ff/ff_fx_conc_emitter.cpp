1fbdd7d4f7080abe206915301846baa713dbf6be
jiggles <jiggles@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2007-09-30 20:18:02 +0000

    I guess these are cheats, too.

--- cl_dll/ff/ff_fx_conc_emitter.cpp
+++ cl_dll/ff/ff_fx_conc_emitter.cpp
@@ -24,18 +24,18 @@
 
 #define CONC_EFFECT_MATERIAL "sprites/concrefract"
 
-ConVar conc_on			 ("ffdev_conc_on", "1", 0, "Turn the conc effect on or off - 1 or 0." );
-ConVar conc_scale		 ("ffdev_conc_scale", "512.0", 0, "How big the conc effect gets.");
-//ConVar conc_refract		 ("ffdev_conc_refract", "0.2", 0, "Refraction amount for conc effect.");
-//ConVar conc_blur		 ("ffdev_conc_blur", "0", 0, "Blur amount for conc effect.");
-ConVar conc_speed		 ("ffdev_conc_speed", "0.5", 0, "Duration of the conc effect.");
-ConVar conc_ripples		 ("ffdev_conc_ripples", "1", 0, "How many ripples the conc effect has.");
-ConVar conc_ripple_period("ffdev_conc_ripple_period", "0.05", 0, "Time between ripples.");
+ConVar conc_on			 ("ffdev_conc_on", "1", FCVAR_CHEAT, "Turn the conc effect on or off - 1 or 0." );
+ConVar conc_scale		 ("ffdev_conc_scale", "512.0", FCVAR_CHEAT, "How big the conc effect gets.");
+//ConVar conc_refract		 ("ffdev_conc_refract", "0.2", FCVAR_CHEAT, "Refraction amount for conc effect.");
+//ConVar conc_blur		 ("ffdev_conc_blur", "0", FCVAR_CHEAT, "Blur amount for conc effect.");
+ConVar conc_speed		 ("ffdev_conc_speed", "0.5", FCVAR_CHEAT, "Duration of the conc effect.");
+ConVar conc_ripples		 ("ffdev_conc_ripples", "1", FCVAR_CHEAT, "How many ripples the conc effect has.");
+ConVar conc_ripple_period("ffdev_conc_ripple_period", "0.05", FCVAR_CHEAT, "Time between ripples.");
 
-ConVar conc_blur("ffdev_conc_blur", "0.5");
-ConVar conc_refract("ffdev_conc_refract", "0.2");
-ConVar conc_grow("ffdev_conc_grow", "0.2");
-ConVar conc_shrink("ffdev_conc_shrink", "1.2");
+ConVar conc_blur("ffdev_conc_blur", "0.5", FCVAR_CHEAT);
+ConVar conc_refract("ffdev_conc_refract", "0.2", FCVAR_CHEAT);
+ConVar conc_grow("ffdev_conc_grow", "0.2", FCVAR_CHEAT);
+ConVar conc_shrink("ffdev_conc_shrink", "1.2", FCVAR_CHEAT);
 
 class CRagdollConcEnumerator : public IPartitionEnumerator
 {
