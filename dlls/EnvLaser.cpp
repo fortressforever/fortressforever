daeb9b3eeb22feed32fb357b0771a7925b4dcfad
mirvin_monkey <mirvin_monkey@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2007-12-10 18:51:13 +0000

    Laser think function is extremely slow, so removing it for now

--- dlls/EnvLaser.cpp
+++ dlls/EnvLaser.cpp
@@ -232,6 +232,8 @@ void CEnvLaser::FireAtPoint( trace_t &tr )
 //-----------------------------------------------------------------------------
 void CEnvLaser::StrikeThink( void )
 {
+	return; // |-- Mirv: Removed this because it is really REALLY slow
+
 	CBaseEntity *pEnd = RandomTargetname( STRING( m_iszLaserTarget ) );
 
 	Vector vecFireAt = GetAbsEndPos();
