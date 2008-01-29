a0f6b037ab7d14b604d81a2c4e7c7ffba6f5631d
Aftershock <Aftershock@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2008-01-29 20:10:25 +0000

    oops

---
+++ game_shared/ff/ff_weapon_assaultcannon.h
@@ -0,0 +1,106 @@
+// =============== Fortress Forever ==============
+// ======== A modification for Half-Life 2 =======
+//
+// @file ff_weapon_assaultcannon.h
+// @author Mike Parker (AfterShock)
+// @date Jan 27th 2008
+// @brief The FF assault cannon code .h file
+//
+
+#ifndef FF_WEAPON_ASSAULTCANNON_H
+#define FF_WEAPON_ASSAULTCANNON_H
+
+#ifdef _WIN32
+#pragma once
+#endif
+
+#include "cbase.h"
+#include "ff_weapon_base.h"
+#include "ff_fx_shared.h"
+#include "in_buttons.h"
+
+#ifdef CLIENT_DLL 
+	#define CFFWeaponAssaultCannon C_FFWeaponAssaultCannon
+	#include "c_ff_player.h"
+	#include "ff_utils.h"
+	#include "soundenvelope.h"
+#else
+	#include "omnibot_interface.h"
+	#include "ff_player.h"
+#endif
+
+#define FF_AC_MAXCHARGETIME 3.0f	// Assault Cannon Max Charge Time
+
+//=============================================================================
+// CFFWeaponAssaultCannon
+//=============================================================================
+
+class CFFWeaponAssaultCannon : public CFFWeaponBase
+{
+public:
+	DECLARE_CLASS(CFFWeaponAssaultCannon, CFFWeaponBase);
+	DECLARE_NETWORKCLASS(); 
+	DECLARE_PREDICTABLE();
+	
+	CFFWeaponAssaultCannon();
+	~CFFWeaponAssaultCannon();
+
+	virtual void Precache();
+	virtual bool Deploy();
+	virtual bool Holster( CBaseCombatWeapon *pSwitchingTo );
+	virtual void Drop( const Vector& vecVelocity );
+	virtual void ToggleClamp();
+	virtual void ItemPostFrame();
+	virtual void PrimaryAttack();
+	//virtual void Fire();
+
+	void UpdateChargeTime();
+
+	virtual void GetHeatLevel(int _firemode, float &_current, float &_max) 
+	{
+		_current = m_flChargeTime; 
+		_max = FF_AC_MAXCHARGETIME;
+	}
+private:
+	virtual float GetFireRate();
+	Vector GetFireSpread();
+	bool m_bClamped;
+
+
+#ifdef CLIENT_DLL
+
+	void UpdateBarrelRotation();
+	void StopBarrelRotationSound();
+	int m_iBarrelRotation;
+	float m_flBarrelRotationValue;
+	float m_flBarrelRotationDelta;
+	float m_flBarrelRotationStopTimer;
+	CSoundPatch *m_sndBarrelRotation;
+
+	void StopLoopShotSound();
+	CSoundPatch *m_sndLoopShot;
+
+	float m_flChargeTimeBuffered;
+	float m_flChargeTimeBufferedNextUpdate;
+
+#endif
+
+	virtual FFWeaponID GetWeaponID() const		{ return FF_WEAPON_ASSAULTCANNON; }
+	const char *GetTracerType() { return "AR2Tracer"; }
+
+private:
+
+	CFFWeaponAssaultCannon(const CFFWeaponAssaultCannon &);
+
+public:	// temp while i expose m_flChargeTime to global function
+
+	float m_flLastTick;
+	float m_flDeployTick;
+	CNetworkVar(float, m_flTriggerPressed);
+	CNetworkVar(float, m_flTriggerReleased);
+	bool m_bFiring;
+	float m_flChargeTime;
+	float m_flMaxChargeTime;
+};
+
+#endif // FF_WEAPON_ASSAULTCANNON_H
