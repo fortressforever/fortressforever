7bb4a06b911e3d65624b1fe0075de6821d6cffa5
jiggles <jiggles@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2008-01-30 23:57:06 +0000

    Reduced conc time for Scout down to Medic level; Scouts now don't take fall damage.

--- game_shared/ff/ff_gamerules.cpp
+++ game_shared/ff/ff_gamerules.cpp
@@ -980,6 +980,10 @@ ConVar mp_prematch( "mp_prematch",
 			return 9999;
 		}
 
+		// Jiggles: Scouts don't take fall damage; they're just cool like that
+		if ( pFFPlayer->GetClassSlot() == CLASS_SCOUT )
+			return 0;
+
 		float flMaxSafe = PLAYER_MAX_SAFE_FALL_SPEED;
 
 		bool bIsSpy = (pFFPlayer->GetClassSlot() == 8);
