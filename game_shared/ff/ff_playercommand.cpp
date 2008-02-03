40717b7b25e2dbbac3e8d892dae8ed7e5e347306
AfterShock <mikkey@gmail.com>
2008-02-03 02:27:42 +0000

    removed radar command, and the sevtest thing also.

--- game_shared/ff/ff_playercommand.cpp
+++ game_shared/ff/ff_playercommand.cpp
@@ -153,9 +153,9 @@ FF_AUTO_COMMAND( dispensertext, &CFFPlayer::Command_DispenserText, "Set custom t
 FF_AUTO_COMMAND( hinttest, &CFFPlayer::Command_HintTest, "Tests the hint system.", FF_CMD_ALIVE | FF_CMD_DEAD | FF_CMD_SPEC | FF_CMD_PREMATCH );
 FF_AUTO_COMMAND( detpack, &CFFPlayer::Command_BuildDetpack, "Drop a detpack.", FF_CMD_SKILL_COMMAND | FF_CMD_ALIVE | FF_CMD_PREMATCH );
 FF_AUTO_COMMAND( mancannon, &CFFPlayer::Command_BuildManCannon, "Build a man cannon.", FF_CMD_SKILL_COMMAND | FF_CMD_ALIVE | FF_CMD_PREMATCH );
-FF_AUTO_COMMAND( radar, &CFFPlayer::Command_Radar, "Start the scout radar.", FF_CMD_SKILL_COMMAND | FF_CMD_ALIVE | FF_CMD_PREMATCH );
+//FF_AUTO_COMMAND( radar, &CFFPlayer::Command_Radar, "Start the scout radar.", FF_CMD_SKILL_COMMAND | FF_CMD_ALIVE | FF_CMD_PREMATCH );
 // For sev's animation testing thing
-FF_AUTO_COMMAND( sevtest, &CFFPlayer::Command_SevTest, "Spawn sev's testing thing", FF_CMD_ALIVE  | FF_CMD_PREMATCH );
+//FF_AUTO_COMMAND( sevtest, &CFFPlayer::Command_SevTest, "Spawn sev's testing thing", FF_CMD_ALIVE  | FF_CMD_PREMATCH );
 FF_AUTO_COMMAND( discard, &CFFPlayer::Command_Discard, "Discards unneeded ammo", FF_CMD_ALIVE | FF_CMD_PREMATCH | FF_CMD_CLOAKED );
 FF_AUTO_COMMAND( saveme, &CFFPlayer::Command_SaveMe, "Call for medical attention", FF_CMD_ALIVE | FF_CMD_PREMATCH );
 FF_AUTO_COMMAND( engyme, &CFFPlayer::Command_EngyMe, "Call for engineer attention", FF_CMD_ALIVE | FF_CMD_PREMATCH );
