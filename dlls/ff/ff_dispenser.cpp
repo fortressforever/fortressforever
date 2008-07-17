bb55ad3f1f54a27501c28e0dad9ffbb17e9fa5b1
trepids <trepids@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2008-07-17 03:23:54 +0000

    - dispenser: put ammo in on initial build as well as doubled ammo values and halved touch & regen times - sentry: made ffdev_sg_range_untarget 50% more than the regular range as opposed to only 10% more

--- dlls/ff/ff_dispenser.cpp
+++ dlls/ff/ff_dispenser.cpp
@@ -247,7 +247,7 @@ void CFFDispenser::OnObjectTouch( CBaseEntity *pOther )
 					if( pPlayer == m_pLastTouch )
 					{
 						// If the player hasn't touched us within some time...
-						if( ( m_flLastTouch + 2.0f ) < gpGlobals->curtime )
+						if( ( m_flLastTouch + 1.0f ) < gpGlobals->curtime )
 						{
 							bDispense = true;
 						}
@@ -349,11 +349,11 @@ void CFFDispenser::OnObjectThink( void )
 	CheckForOwner();
 
 	// Generate stock
-	m_iShells = clamp( m_iShells + 20, 0, m_iMaxShells );
-	m_iNails = clamp( m_iNails + 30, 0, m_iMaxNails );
-	m_iCells = clamp( m_iCells + 20, 0, m_iMaxCells );
-	m_iRockets = clamp( m_iRockets + 10, 0, m_iMaxRockets );
-	m_iArmor = clamp( m_iArmor + 50, 0, m_iMaxArmor );
+	m_iCells = clamp( m_iCells + 40, 0, m_iMaxCells );
+	m_iNails = clamp( m_iNails + 60, 0, m_iMaxNails );
+	m_iShells = clamp( m_iShells + 40, 0, m_iMaxShells );
+	m_iRockets = clamp( m_iRockets + 20, 0, m_iMaxRockets );
+	m_iArmor = clamp( m_iArmor + 100, 0, m_iMaxArmor );
 
 	// Update ammo percentage
 	UpdateAmmoPercentage();	
