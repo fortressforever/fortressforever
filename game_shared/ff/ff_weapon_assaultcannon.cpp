000a651d05c46ed64ab679ecb8358b199956b455
trepids <trepids@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2009-01-27 05:57:50 +0000

    just some minor #ifdef stuff

--- game_shared/ff/ff_weapon_assaultcannon.cpp
+++ game_shared/ff/ff_weapon_assaultcannon.cpp
@@ -26,10 +26,13 @@
 									// AfterShock: I'm also using this for the 'charge down' time now also (time before bullets stop firing, after you stop pressing fire)
 #define FF_AC_MINCLAMPTIME 0.53f	// minimum charge on the AC before you can clamp
 
-//ConVar ffdev_ac_tracerfreq_server( "ffdev_ac_tracerfreq_server", "8", FCVAR_REPLICATED, "Tracers display on 1 in X bullets" );
-#define FF_AC_TRACERFREQ_SERVER 8 // ffdev_ac_tracerfreq_server.GetInt()
-//ConVar ffdev_ac_tracerfreq_client( "ffdev_ac_tracerfreq_client", "2", FCVAR_REPLICATED, "Tracers display on 1 in X bullets" );
-#define FF_AC_TRACERFREQ_CLIENT 2 // ffdev_ac_tracerfreq_client.GetInt()
+#ifdef GAME_DLL
+	//ConVar ffdev_ac_tracerfreq_server( "ffdev_ac_tracerfreq_server", "8", FCVAR_REPLICATED, "Tracers display on 1 in X bullets" );
+	#define FF_AC_TRACERFREQ_SERVER 8 // ffdev_ac_tracerfreq_server.GetInt()
+#else
+	//ConVar ffdev_ac_tracerfreq_client( "ffdev_ac_tracerfreq_client", "2", FCVAR_REPLICATED, "Tracers display on 1 in X bullets" );
+	#define FF_AC_TRACERFREQ_CLIENT 2 // ffdev_ac_tracerfreq_client.GetInt()
+#endif
 
 //ConVar ffdev_ac_spread_min( "ffdev_ac_spread_min", "0.06", FCVAR_REPLICATED, "The minimum cone of fire spread for the AC" );
 #define FF_AC_SPREAD_MIN 0.06f // ffdev_ac_spread_min.GetFloat()
