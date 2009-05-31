7781e0f1b02d4bc2da8c2df9add5d8b49e4ad34c
squeek <squeek502@hotmail.com>
2009-05-31 00:58:21 +0000

    Finished merge of addhealth/addarmor

---
+++ cl_dll/ff/ff_hud_addarmor.cpp
@@ -0,0 +1,181 @@
+
+
+//	=============== Fortress Forever ==============
+//	======== A modification for Half-Life 2 =======
+//
+//	@file ff_hud_scorelatest.cpp
+//	@author Michael Parker (AfterShock)
+//	@date 30/06/2007
+//	@brief Hud Player Armor field - with details of your latest score
+//
+//	REVISIONS
+//	---------
+//	30/06/2007, AfterShock: 
+//		First created (from ff_hud_spydisguise.cpp)
+
+#include "cbase.h"
+#include "hud.h"
+#include "hudelement.h"
+#include "hud_macros.h"
+
+//#include <KeyValues.h>
+#include <vgui/ISurface.h>
+#include <vgui/ISystem.h>
+
+#include "ff_panel.h"
+#include "c_ff_player.h"
+#include "ff_utils.h"
+#include "c_playerresource.h"
+
+#include <vgui/ILocalize.h>
+
+#include "G15.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+using namespace vgui;
+static ConVar hud_addarmor("hud_addarmor", "1", FCVAR_CLIENTDLL | FCVAR_ARCHIVE, "Toggle visibility of added armor notices.");
+
+//-----------------------------------------------------------------------------
+// Purpose: Displays current disguised class
+//-----------------------------------------------------------------------------
+class CHudPlayerAddArmor : public CHudElement, public vgui::FFPanel
+{
+public:
+	DECLARE_CLASS_SIMPLE( CHudPlayerAddArmor, vgui::FFPanel );
+
+	CHudPlayerAddArmor( const char *pElementName ) : vgui::FFPanel( NULL, "HudPlayerAddArmor" ), CHudElement( pElementName )
+	{
+		// Set our parent window
+		SetParent( g_pClientMode->GetViewport() );
+
+		// Hide when player is dead
+		SetHiddenBits( HIDEHUD_PLAYERDEAD );
+	}
+
+	virtual ~CHudPlayerAddArmor( void )
+	{
+
+	}
+
+	virtual void Paint( void );
+	virtual void Init( void );
+	virtual void VidInit( void );
+	virtual bool ShouldDraw( void );
+	void MsgFunc_PlayerAddArmor( bf_read &msg );
+
+protected:
+	wchar_t		m_pTextArmor[ 1024 ];	// Unicode text buffer
+	int			m_iArmor;
+
+private:
+	// Stuff we need to know	
+	CPanelAnimationVar( vgui::HFont, m_hArmorFont, "ArmorFont", "Default" );
+
+	CPanelAnimationVar( vgui::HFont, m_hArmorFontBG, "ArmorFontBG", "Default" );
+
+	CPanelAnimationVarAliasType( float, ArmorFont_xpos, "ArmorFont_xpos", "0", "proportional_float" );
+	CPanelAnimationVarAliasType( float, ArmorFont_ypos, "ArmorFont_ypos", "0", "proportional_float" );
+};
+
+DECLARE_HUDELEMENT( CHudPlayerAddArmor );
+DECLARE_HUD_MESSAGE( CHudPlayerAddArmor, PlayerAddArmor );
+
+//-----------------------------------------------------------------------------
+// Purpose: Done on loading game?
+//-----------------------------------------------------------------------------
+void CHudPlayerAddArmor::Init( void )
+{
+	HOOK_HUD_MESSAGE( CHudPlayerAddArmor, PlayerAddArmor );
+
+	m_pTextArmor[ 0 ] = '\0';
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Done each map load
+//-----------------------------------------------------------------------------
+void CHudPlayerAddArmor::VidInit( void )
+{
+	SetPaintBackgroundEnabled( false );
+	
+	m_pTextArmor[ 0 ] = '\0'; 
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Should we draw? (Are we ingame? have we picked a class, etc)
+//-----------------------------------------------------------------------------
+bool CHudPlayerAddArmor::ShouldDraw() 
+{ 
+	if( !engine->IsInGame() ) 
+		return false; 
+
+	C_FFPlayer *pPlayer = C_FFPlayer::GetLocalFFPlayer(); 
+
+	if( !pPlayer ) 
+		return false; 
+
+	if( FF_IsPlayerSpec( pPlayer ) || !FF_HasPlayerPickedClass( pPlayer ) ) 
+		return false; 
+
+	return true; 
+} 
+
+void CHudPlayerAddArmor::MsgFunc_PlayerAddArmor( bf_read &msg )
+{
+	// Read int and convert to string
+	const int ptVal = msg.ReadShort();
+	if(ptVal==0)
+		return;
+
+	char szString2[ 1024 ];
+	Q_snprintf( szString2, sizeof(szString2), "%s%i", ptVal>0?"+":"",ptVal );
+
+	// convert int-string to unicode
+	localize()->ConvertANSIToUnicode( szString2, m_pTextArmor, sizeof( m_pTextArmor ) );
+
+	// play animation (new points value)
+	if(ptVal > 0)
+	{
+		g_pClientMode->GetViewportAnimationController()->StartAnimationSequence( "NewAddArmor" );
+	}
+	else
+	{
+		g_pClientMode->GetViewportAnimationController()->StartAnimationSequence( "NewSubtractArmor" );
+	}
+
+	//DevMsg( "[Location] Team: %i, String: %s\n", iTeam, szString );
+	if(G15::IsEnabled())
+	{
+		enum { BufferSize = 1024 };
+		char score[BufferSize] = {0};
+		localize()->ConvertUnicodeToANSI(m_pTextArmor, score, BufferSize);
+		//G15::AddFortPoints(description, score);
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Draw stuff!
+//-----------------------------------------------------------------------------
+void CHudPlayerAddArmor::Paint() 
+{
+	C_FFPlayer *pPlayer = C_FFPlayer::GetLocalFFPlayer(); 
+	if ( !pPlayer ) 
+		return; 
+
+	if(!hud_addarmor.GetBool())
+		return;
+
+	FFPanel::Paint(); // Draws the background glyphs 
+
+	if( m_pTextArmor[ 0 ] != '\0' )
+	{
+		surface()->DrawSetTextFont( m_hArmorFont );
+		surface()->DrawSetTextColor( GetFgColor() );
+		surface()->DrawSetTextPos( ArmorFont_xpos, ArmorFont_ypos );
+
+		for( wchar_t *wch = m_pTextArmor; *wch != 0; wch++ )
+			surface()->DrawUnicodeChar( *wch );
+
+	}
+}
---
+++ cl_dll/ff/ff_hud_addhealth.cpp
@@ -0,0 +1,181 @@
+
+
+//	=============== Fortress Forever ==============
+//	======== A modification for Half-Life 2 =======
+//
+//	@file ff_hud_scorelatest.cpp
+//	@author Michael Parker (AfterShock)
+//	@date 30/06/2007
+//	@brief Hud Player Health field - with details of your latest score
+//
+//	REVISIONS
+//	---------
+//	30/06/2007, AfterShock: 
+//		First created (from ff_hud_spydisguise.cpp)
+
+#include "cbase.h"
+#include "hud.h"
+#include "hudelement.h"
+#include "hud_macros.h"
+
+//#include <KeyValues.h>
+#include <vgui/ISurface.h>
+#include <vgui/ISystem.h>
+
+#include "ff_panel.h"
+#include "c_ff_player.h"
+#include "ff_utils.h"
+#include "c_playerresource.h"
+
+#include <vgui/ILocalize.h>
+
+#include "G15.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+using namespace vgui;
+static ConVar hud_addhealth("hud_addhealth", "1", FCVAR_CLIENTDLL | FCVAR_ARCHIVE, "Toggle visibility of added health notices.");
+
+//-----------------------------------------------------------------------------
+// Purpose: Displays current disguised class
+//-----------------------------------------------------------------------------
+class CHudPlayerAddHealth : public CHudElement, public vgui::FFPanel
+{
+public:
+	DECLARE_CLASS_SIMPLE( CHudPlayerAddHealth, vgui::FFPanel );
+
+	CHudPlayerAddHealth( const char *pElementName ) : vgui::FFPanel( NULL, "HudPlayerAddHealth" ), CHudElement( pElementName )
+	{
+		// Set our parent window
+		SetParent( g_pClientMode->GetViewport() );
+
+		// Hide when player is dead
+		SetHiddenBits( HIDEHUD_PLAYERDEAD );
+	}
+
+	virtual ~CHudPlayerAddHealth( void )
+	{
+
+	}
+
+	virtual void Paint( void );
+	virtual void Init( void );
+	virtual void VidInit( void );
+	virtual bool ShouldDraw( void );
+	void MsgFunc_PlayerAddHealth( bf_read &msg );
+
+protected:
+	wchar_t		m_pTextHealth[ 1024 ];	// Unicode text buffer
+	int			m_iHealth;
+
+private:
+	// Stuff we need to know	
+	CPanelAnimationVar( vgui::HFont, m_hHealthFont, "HealthFont", "Default" );
+
+	CPanelAnimationVar( vgui::HFont, m_hHealthFontBG, "HealthFontBG", "Default" );
+
+	CPanelAnimationVarAliasType( float, HealthFont_xpos, "HealthFont_xpos", "0", "proportional_float" );
+	CPanelAnimationVarAliasType( float, HealthFont_ypos, "HealthFont_ypos", "0", "proportional_float" );
+};
+
+DECLARE_HUDELEMENT( CHudPlayerAddHealth );
+DECLARE_HUD_MESSAGE( CHudPlayerAddHealth, PlayerAddHealth );
+
+//-----------------------------------------------------------------------------
+// Purpose: Done on loading game?
+//-----------------------------------------------------------------------------
+void CHudPlayerAddHealth::Init( void )
+{
+	HOOK_HUD_MESSAGE( CHudPlayerAddHealth, PlayerAddHealth );
+
+	m_pTextHealth[ 0 ] = '\0';
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Done each map load
+//-----------------------------------------------------------------------------
+void CHudPlayerAddHealth::VidInit( void )
+{
+	SetPaintBackgroundEnabled( false );
+	
+	m_pTextHealth[ 0 ] = '\0'; 
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Should we draw? (Are we ingame? have we picked a class, etc)
+//-----------------------------------------------------------------------------
+bool CHudPlayerAddHealth::ShouldDraw() 
+{ 
+	if( !engine->IsInGame() ) 
+		return false; 
+
+	C_FFPlayer *pPlayer = C_FFPlayer::GetLocalFFPlayer(); 
+
+	if( !pPlayer ) 
+		return false; 
+
+	if( FF_IsPlayerSpec( pPlayer ) || !FF_HasPlayerPickedClass( pPlayer ) ) 
+		return false; 
+
+	return true; 
+} 
+
+void CHudPlayerAddHealth::MsgFunc_PlayerAddHealth( bf_read &msg )
+{
+	// Read int and convert to string
+	const int ptVal = msg.ReadShort();
+	if(ptVal==0)
+		return;
+
+	char szString2[ 1024 ];
+	Q_snprintf( szString2, sizeof(szString2), "%s%i", ptVal>0?"+":"",ptVal );
+
+	// convert int-string to unicode
+	localize()->ConvertANSIToUnicode( szString2, m_pTextHealth, sizeof( m_pTextHealth ) );
+
+	// play animation (new points value)
+	if(ptVal > 0)
+	{
+		g_pClientMode->GetViewportAnimationController()->StartAnimationSequence( "NewAddHealth" );
+	}
+	else
+	{
+		g_pClientMode->GetViewportAnimationController()->StartAnimationSequence( "NewSubtractHealth" );
+	}
+
+	//DevMsg( "[Location] Team: %i, String: %s\n", iTeam, szString );
+	if(G15::IsEnabled())
+	{
+		enum { BufferSize = 1024 };
+		char score[BufferSize] = {0};
+		localize()->ConvertUnicodeToANSI(m_pTextHealth, score, BufferSize);
+		//G15::AddFortPoints(description, score);
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Draw stuff!
+//-----------------------------------------------------------------------------
+void CHudPlayerAddHealth::Paint() 
+{
+	C_FFPlayer *pPlayer = C_FFPlayer::GetLocalFFPlayer(); 
+	if ( !pPlayer ) 
+		return; 
+
+	if(!hud_addhealth.GetBool())
+		return;
+
+	FFPanel::Paint(); // Draws the background glyphs 
+
+	if( m_pTextHealth[ 0 ] != '\0' )
+	{
+		surface()->DrawSetTextFont( m_hHealthFont );
+		surface()->DrawSetTextColor( GetFgColor() );
+		surface()->DrawSetTextPos( HealthFont_xpos, HealthFont_ypos );
+
+		for( wchar_t *wch = m_pTextHealth; *wch != 0; wch++ )
+			surface()->DrawUnicodeChar( *wch );
+
+	}
+}
