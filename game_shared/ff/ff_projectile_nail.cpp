4c83bb2af271a497b5bedf473971fba12c5811b9
AfterShock <AfterShock@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2009-06-21 23:51:13 +0000

    ffdev_nail_speed for testing purposes, also no longer a cheat. defaults to 1000 now

--- game_shared/ff/ff_projectile_nail.cpp
+++ game_shared/ff/ff_projectile_nail.cpp
@@ -21,10 +21,10 @@
 
 #define NAIL_MODEL "models/projectiles/nail/w_nail.mdl"
 
-//ConVar ffdev_nail_speed("ffdev_nail_speed", "2000.0", FCVAR_REPLICATED | FCVAR_CHEAT, "Nail speed");
+ConVar ffdev_nail_speed("ffdev_nail_speed", "1000.0", FCVAR_REPLICATED , "Nail speed");
 //ConVar ffdev_nail_pushmultiplier("ffdev_nail_pushmultiplier", "0.05", FCVAR_REPLICATED | FCVAR_CHEAT, "Nail pushforce multiplier - was 0.1 for 2.1 release");
 #define FF_NAIL_PUSHMULTIPLIER 0.05f //ffdev_nail_pushmultiplier.GetFloat()
-#define NAIL_SPEED 2000.0f
+#define NAIL_SPEED ffdev_nail_speed.GetFloat() //2000.0f
 //ConVar ffdev_nail_bbox("ffdev_nail_bbox", "2.0", FCVAR_REPLICATED | FCVAR_CHEAT, "Nail bbox");
 #define NAIL_BBOX 2.0f
 //ConVar ffdev_nail_sgmod( "ffdev_nail_sgmod", "10.0", FCVAR_REPLICATED | FCVAR_CHEAT, "Added to nail damage when hitting a SG so SG's take more damage" );
@@ -195,8 +195,8 @@ CFFProjectileNail *CFFProjectileNail::CreateNail(const CBaseEntity *pSource, con
 	Vector vecForward;
 	AngleVectors(angAngles, &vecForward);
 
-	vecForward *= iSpeed /*NAIL_SPEED*/; //AfterShock - lets go back to having different nail speeds script-side (for 2.3!)
-	//vecForward *= NAIL_SPEED; 
+	//vecForward *= iSpeed /*NAIL_SPEED*/; //AfterShock - lets go back to having different nail speeds script-side (for 2.3!)
+	vecForward *= NAIL_SPEED; 
 
 	// Set the speed and the initial transmitted velocity
 	pNail->SetAbsVelocity(vecForward);
@@ -206,7 +206,7 @@ CFFProjectileNail *CFFProjectileNail::CreateNail(const CBaseEntity *pSource, con
 		CEffectData data;
 		data.m_vOrigin = vecOrigin;
 		data.m_vAngles = angAngles;
-		data.m_nDamageType = iSpeed; // AfterShock: HACK: use m_nDamageType to pass the nail speed int
+		data.m_nDamageType = NAIL_SPEED;//iSpeed; // AfterShock: HACK: use m_nDamageType to pass the nail speed int
 
 	#ifdef GAME_DLL
 		data.m_nEntIndex = pentOwner->entindex();
