2a56108aa1f6be63c8c48a05ea393936027af4b9
AfterShock <mikkey@gmail.com>
2009-07-29 22:41:07 +0000

    Removed nailspeed cvar, set to 1000

--- game_shared/ff/ff_projectile_nail.cpp
+++ game_shared/ff/ff_projectile_nail.cpp
@@ -21,10 +21,10 @@
 
 #define NAIL_MODEL "models/projectiles/nail/w_nail.mdl"
 
-ConVar ffdev_nail_speed("ffdev_nail_speed", "1000.0", FCVAR_REPLICATED , "Nail speed");
+//ConVar ffdev_nail_speed("ffdev_nail_speed", "1000.0", FCVAR_REPLICATED , "Nail speed");
 //ConVar ffdev_nail_pushmultiplier("ffdev_nail_pushmultiplier", "0.05", FCVAR_REPLICATED | FCVAR_CHEAT, "Nail pushforce multiplier - was 0.1 for 2.1 release");
 #define FF_NAIL_PUSHMULTIPLIER 0.05f //ffdev_nail_pushmultiplier.GetFloat()
-#define NAIL_SPEED ffdev_nail_speed.GetFloat() //2000.0f
+#define NAIL_SPEED 1000.0f //ffdev_nail_speed.GetFloat() //2000.0f
 //ConVar ffdev_nail_bbox("ffdev_nail_bbox", "2.0", FCVAR_REPLICATED | FCVAR_CHEAT, "Nail bbox");
 #define NAIL_BBOX 2.0f
 //ConVar ffdev_nail_sgmod( "ffdev_nail_sgmod", "10.0", FCVAR_REPLICATED | FCVAR_CHEAT, "Added to nail damage when hitting a SG so SG's take more damage" );
