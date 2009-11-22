578a57687805b14e0c33f82c5554cf8a3bca9ec0
squeek <squeek.@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2009-11-22 23:17:29 +0000

    Partial merge 11387 (no SG explosion damage)

--- dlls/ff/ff_sentrygun.cpp
+++ dlls/ff/ff_sentrygun.cpp
@@ -1877,10 +1877,12 @@ void CFFSentryGun::Detonate()
 // Purpose: Carry out the radius damage for this buildable
 //-----------------------------------------------------------------------------
 void CFFSentryGun::DoExplosionDamage()
-{
+{	
+	/* No more SG explosion damage! Engineers rejoice!
 	VPROF_BUDGET( "CFFSentryGun::DoExplosionDamage", VPROF_BUDGETGROUP_FF_BUILDABLE );
 
-	float flDamage = SG_EXPLOSIONDAMAGE_BASE * m_iLevel  + (m_iRockets * 1.4f);
+	//float flDamage = SG_EXPLOSIONDAMAGE_BASE * m_iLevel  + (m_iRockets * 1.4f);
+	float flDamage = SG_EXPLOSIONDAMAGE_BASE * m_iLevel;
 	// COmmented out for testing explosion damage - AfterShock
 	//flDamage = min(280, flDamage);
 	
@@ -1892,6 +1894,7 @@ void CFFSentryGun::DoExplosionDamage()
 
 		UTIL_ScreenShake(GetAbsOrigin(), flDamage * 0.0125f, 150.0f, m_flExplosionDuration, 620.0f, SHAKE_START);
 	}
+	*/
 }
 
 //-----------------------------------------------------------------------------
