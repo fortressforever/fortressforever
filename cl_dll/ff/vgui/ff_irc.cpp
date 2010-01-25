9da83b6cf980103a0dc8c5cbc01c6c6ef641f1e5
squeek <squeek502@hotmail.com>
2010-01-25 10:02:35 +0000

    Rather large update to the IRC code (added sockets, a thread, and parsing for some commands)

--- cl_dll/ff/vgui/ff_irc.cpp
+++ cl_dll/ff/vgui/ff_irc.cpp
@@ -1,7 +1,28 @@
+
 #include "cbase.h"
 #include "ff_irc.h"
+
+#include "irc/ff_socks.h"
+#include "tier0/threadtools.h"
+
 // memdbgon must be the last include file in a .cpp file!!! 
 #include "tier0/memdbgon.h"
+
+using namespace vgui;
+
+//threadtest.h
+Socks irc_socket;
+
+CFFIRCPanel *g_pIRCPanel = NULL;
+
+struct cUser
+{
+	char nick[30]; 
+	char ident[100];
+	char email[100];
+	char status;
+};
+cUser irc_user;
 
 //-----------------------------------------------------------------------------
 // CFFIRCLobbyTab
@@ -15,30 +36,49 @@ public:
 	
 	CFFIRCLobbyTab(Panel *parent, char const *panelName) : BaseClass(parent, panelName)
 	{
-		m_pTextEntry_ChatEntry = new vgui::TextEntry(this, "TextEntry_ChatEntry");
-		m_pRichText_LobbyChat = new vgui::RichText(this, "RichText_LobbyChat");
-		m_pRichText_LobbyUserList = new vgui::RichText(this, "RichText_LobbyUserList");
+		m_pTextEntry_NickEntry = new vgui::TextEntry(this, "TextEntry_NickEntry");
+		m_pTextEntry_NickEntry->SetText("tempnick123");
 
-		m_pTextEntry_ChatEntry->SendNewLine(true); // Pressing enter in the text box triggers a new line message
-		m_pTextEntry_ChatEntry->AddActionSignalTarget( this ); // Something about sending messages to the right panel?
+		m_pGameList = new ListPanel(this, "ListPanel_GameList");
+
+		m_pGameList->AddActionSignalTarget( this );
+		
+        m_pGameList->AddColumnHeader( 0, "players" , "Players" , 20, ListPanel::COLUMN_RESIZEWITHWINDOW );
+        m_pGameList->AddColumnHeader( 1, "name" , "Game Name" , 200, ListPanel::COLUMN_RESIZEWITHWINDOW );
+		m_pGameList->AddColumnHeader( 2, "map" , "Map Name" , 150, ListPanel::COLUMN_RESIZEWITHWINDOW  );	// |-- Mirv: Current class
+
+		KeyValues *kv = new KeyValues( "LI" );
+		kv->SetString( "players", "1/8" );
+		kv->SetString( "name", "4v4 1500+ pros" );
+		kv->SetString( "map", "ff_monkey" );
+		m_pGameList->AddItem( kv, 0, false, false );
+		kv->deleteThis();
 
 		LoadControlSettings("resource/ui/FFIRCLobbyTab.res");
 	}
 	
-private:
+	virtual void ApplySchemeSettings( IScheme *pScheme )
+	{
+		BaseClass::ApplySchemeSettings( pScheme );
 
+		m_pGameList->SetFont( pScheme->GetFont( "DefaultVerySmall" ) );
+	}
+	
 	MESSAGE_FUNC_PARAMS(OnNewLineMessage, "TextNewLine", data)
 	{
 		char szCommand[256];
 		m_pTextEntry_ChatEntry->GetText(szCommand, sizeof(szCommand));
+		
+		if ( !irc_socket.Send( VarArgs("PRIVMSG #fortressforever :%s\r\n", szCommand) ) )
+			Msg("[IRC] Unable to send message: %s\n", szCommand);
 	                 
 		//const char* text = data->GetString("text");
-		m_pRichText_LobbyChat->InsertString(szCommand);
-		m_pRichText_LobbyChat->InsertString("\n");
+		m_pRichText_Chat->InsertString( VarArgs("%s: %s",irc_user.nick, szCommand) );
+		m_pRichText_Chat->InsertString("\n");
 		m_pTextEntry_ChatEntry->SetText("");
-		m_pRichText_LobbyChat->GotoTextEnd();
+		m_pRichText_Chat->GotoTextEnd();
 	}
-	
+
 	MESSAGE_FUNC_PARAMS(OnButtonCommand, "Command", data)
 	{
 		const char *pszCommand = data->GetString("command");
@@ -52,11 +92,41 @@ private:
 				parent->AddGameTab();
 			}
 		}
+		if (Q_strcmp(pszCommand, "Connect") == 0)
+		{
+			char szNick[30];
+			m_pTextEntry_NickEntry->GetText(szNick, sizeof(szNick));
+			sprintf(irc_user.nick, szNick);
+
+			// Connect to remote host
+			if (!irc_socket.Connect("irc.gamesurge.net", 6667)) 
+			{
+				Msg("[IRC] Could not connect to server\n");
+			}
+			
+			// Send data
+			if (!irc_socket.Send( VarArgs("USER %s %s: %s %s  \n\r", irc_user.nick , irc_user.email , irc_user.ident , irc_user.ident) ))
+			{
+				Msg("[IRC] Could not send USER to server\n");
+				irc_socket.Close();
+			}
+
+			// Send data
+			if (!irc_socket.Send( VarArgs("NICK %s\n\r", irc_user.nick) )) 
+			{
+				Msg("[IRC] Could not send NICK to server\n");
+				irc_socket.Close();
+			}
+		}
+		if (Q_strcmp(pszCommand, "JoinChannel") == 0)
+		{
+			if (!irc_socket.Send( VarArgs("JOIN %s\n\r", "#fortressforever") ))
+				Msg("[IRC] Could not send JOIN to server\n");
+		}
 	}
 
-	vgui::TextEntry			*m_pTextEntry_ChatEntry;
-	vgui::RichText			*m_pRichText_LobbyChat;
-	vgui::RichText			*m_pRichText_LobbyUserList;
+	vgui::ListPanel			*m_pGameList;
+	vgui::TextEntry			*m_pTextEntry_NickEntry;
 
 };
 
@@ -72,28 +142,23 @@ public:
 	
 	CFFIRCGameTab(Panel *parent, char const *panelName) : BaseClass(parent, panelName)
 	{
-		m_pTextEntry_ChatEntry = new vgui::TextEntry(this, "TextEntry_ChatEntry");
-		m_pRichText_GameChat = new vgui::RichText(this, "RichText_GameChat");
-		m_pRichText_GameUserList = new vgui::RichText(this, "RichText_GameUserList");
-
-		m_pTextEntry_ChatEntry->SendNewLine(true); // Pressing enter in the text box triggers a new line message
-		m_pTextEntry_ChatEntry->AddActionSignalTarget( this ); // Something about sending messages to the right panel?
 
 		LoadControlSettings("resource/ui/FFIRCGameTab.res");
 	}
-	
-private:
 
 	MESSAGE_FUNC_PARAMS(OnNewLineMessage, "TextNewLine", data)
 	{
 		char szCommand[256];
 		m_pTextEntry_ChatEntry->GetText(szCommand, sizeof(szCommand));
-	                 
+	    
+		if ( !irc_socket.Send( VarArgs("%s\r\n", szCommand) ) )
+			Msg("[IRC] Unable to send message: %s\n", szCommand);
+
 		//const char* text = data->GetString("text");
-		m_pRichText_GameChat->InsertString(szCommand);
-		m_pRichText_GameChat->InsertString("\n");
+		m_pRichText_Chat->InsertString(szCommand);
+		m_pRichText_Chat->InsertString("\n");
 		m_pTextEntry_ChatEntry->SetText("");
-		m_pRichText_GameChat->GotoTextEnd();
+		m_pRichText_Chat->GotoTextEnd();
 	}
 	
 	MESSAGE_FUNC_PARAMS(OnButtonCommand, "Command", data)
@@ -110,10 +175,6 @@ private:
 			}
 		}
 	}
-
-	vgui::TextEntry			*m_pTextEntry_ChatEntry;
-	vgui::RichText			*m_pRichText_GameChat;
-	vgui::RichText			*m_pRichText_GameUserList;
 
 };
 
@@ -140,6 +201,9 @@ CFFIRCPanel::CFFIRCPanel( vgui::VPANEL parent ) : BaseClass( NULL, "FFIRCPanel" 
 	vgui::HScheme scheme = vgui::scheme()->LoadSchemeFromFile("resource/SourceScheme.res", "SourceScheme");
 	SetScheme( scheme );
 
+	if (g_pIRCPanel == NULL)
+		g_pIRCPanel = this;
+
 	// Centre this panel on the screen for consistency.
 	// This should be in the .res surely? - AfterShock
 	//int nWide = GetWide();
@@ -148,38 +212,76 @@ CFFIRCPanel::CFFIRCPanel( vgui::VPANEL parent ) : BaseClass( NULL, "FFIRCPanel" 
 	//SetPos((ScreenWidth() - nWide) / 2, (ScreenHeight() - nTall) / 2);
 
 	m_pLobbyTab = new CFFIRCLobbyTab(this, "IRCLobbyTab");
-	//m_pGameTab = new CFFIRCGameTab(this, "IRCGameTab");
-	//m_pLobbyTab = new PropertyPage(this, "IRCLobbyTab");
-	//m_pGame1Tab = new PropertyPage(this, "IRCGameTab");
 
-	m_pIRCTabs = new PropertySheet(this, "IRCTabs", true);
+	m_pIRCTabs = new vgui::PropertySheet(this, "IRCTabs", true);
 	m_pIRCTabs->AddPage(m_pLobbyTab, "Lobby");
-	//m_pIRCTabs->AddPage(m_pGame1Tab, "#GameUI_IRCGameTab");
-	//m_pIRCTabs->AddPage(m_pGameTab, "Game1");
 	m_pIRCTabs->SetActivePage(m_pLobbyTab);
 	m_pIRCTabs->SetDragEnabled(false);
 
-	//m_pTextEntryChat1->SetPos(150, 310);
-	//m_pTextEntryChat1->SetSize(100, 20);
+	/*
+	// need to be on a server for there to be a local player... :(
+	C_BasePlayer *pPlayer =	C_BasePlayer::GetLocalPlayer();
 
-	//SetPos(10,10);
-	//SetSize(500, 400);
-	//m_pTextEntryChat1->SetPos
+	player_info_t sPlayerInfo;
+	if ( pPlayer )
+	{
+		engine->GetPlayerInfo( pPlayer->entindex(), &sPlayerInfo );
 
-	//m_pOKButton = new Button(this, "PlayButton", "", this, "Play");
-	//m_pCancelButton = new Button(this, "CancelButton", "", this, "Cancel");
+		const char *pName = sPlayerInfo.name;
 
-	SetSizeable(false);
+		sprintf(irc_user.nick, "%s", pName);
+		Msg("[IRC] Nick: %s / %s\n", irc_user.nick, sPlayerInfo.name );
+		Msg("[IRC] GUID: %s UID: %s friendsID: %d\n", sPlayerInfo.guid, sPlayerInfo.userID, sPlayerInfo.friendsID );
+	}
 
+	engine->GetPlayerInfo( engine->GetLocalPlayer(), &sPlayerInfo );
+
+	Msg("ROUND 2\n", irc_user.nick, sPlayerInfo.name );
+	Msg("[IRC] Nick: %s \n", sPlayerInfo.name );
+	Msg("[IRC] GUID: %s UID: %s friendsID: %d\n", sPlayerInfo.guid, sPlayerInfo.userID, sPlayerInfo.friendsID );
+	*/
+
+	// IRC
+	// start with some base info
+	sprintf(irc_user.nick, "temptest-ff");
+	sprintf(irc_user.ident,"test");
+	sprintf(irc_user.email,"vertexar@yahoo.com");
+
+	// create the response thread
+	CThreadTest::GetInstance();
+
+	// Open up a socket
+	if (!irc_socket.Open(/*SOCK_STREAM */ 1, 0)) 
+	{
+		Msg("[IRC] Could not open socket\n");
+	}
 
 	LoadControlSettings("Resource/UI/FFIRC.res");
 	//CenterThisPanelOnScreen();//keep in mind, hl2 supports widescreen 
-	SetVisible(false);//made visible on command later 
+	SetVisible(false); //made visible on command later 
 
 	//Other useful options
-	//SetSizeable(false);
+	SetSizeable(false);
 	//SetMoveable(false);
-} 
+}
+
+// The main screen where everything IRC related is added to
+CFFIRCPanel::~CFFIRCPanel( )
+{
+	// horrible way to close the thread (I think), but hl2 will crash on close without it - squeek
+	CThreadTest::GetInstance().Terminate();
+	irc_socket.Close();
+}
+
+void CFFIRCPanel::Close()
+{
+	//CThreadTest::GetInstance().Terminate();
+	BaseClass::Close();
+	
+	// leave the main lobby
+	if (!irc_socket.Send( VarArgs("PART %s\n\r", "#fortressforever") ))
+		Msg("[IRC] Could not send PART to server\n");
+}
 
 void CFFIRCPanel::AddGameTab()
 {
@@ -190,4 +292,387 @@ void CFFIRCPanel::AddGameTab()
 void CFFIRCPanel::RemoveGameTab( CFFIRCTab *pTab )
 {
 	m_pIRCTabs->DeletePage(pTab);
+}
+
+void CFFIRCPanel::ParseServerMessage( char *buf )
+{
+	// print to the console for now to see whats going on
+	Msg("%s\n", buf);
+
+	// PING?
+	if (!strnicmp(buf,"PING",4)) 
+	{
+		// PONG
+		buf[1] = 'O';
+		irc_socket.Send( VarArgs("%s\r\n", buf) );
+		return;
+	}
+
+	// Names?
+	// :NuclearFallout.WA.US.GameSurge.net 353 squeektest = #FortressForever :squeektest Paft R00Kie GenghisTron Spartacus cjeshjoir- AtLarge mib_splib j3 []Zer0 Sailor_Mercury-sama_NL_id prodigy Hellfish`away @squeek @ChanServ +padawan
+	if (strstr(buf,VarArgs("353 %s", irc_user.nick)) != NULL)
+	{
+		char *pszChan = strchr(buf,'#');
+		if (Q_strnicmp((pszChan+1), "fortressforever", 15) == 0)
+		{
+			// Populate lobby list
+			CFFIRCLobbyTab *tab = dynamic_cast <CFFIRCLobbyTab *> (m_pLobbyTab);
+			if (tab)
+			{
+				char *msg = strchr(pszChan,':');
+
+				tab->UserList_Fill(msg+1);
+				return;
+			}
+		}
+	}
+
+	// standard IRC server messages always start with a colon
+	//		:nick!host@mask COMMAND parameter1 parameter2 ... [:param with spaces]
+	//		:server 000 mynick parameter1 parameter2 ... [:param with spaces]
+	if (buf[0] == ':')
+	{
+		// find position of the first space
+		int firstspace_pos = strcspn(buf, " ");
+		// if there is a space
+		if (buf[firstspace_pos])
+		{
+			// commands shouldn't be bigger than 30 chars, right?
+			char command[30];
+			int paramstart=0;
+
+			// command/response code can be parsed now, they both come after the first space
+			int i=0;
+			command[29] = '\0';
+			for(int ti=firstspace_pos+1;ti<firstspace_pos+30;++ti)
+			{
+				if (buf[ti] == ' ')
+				{
+					command[i] = '\0';
+					// remember this place so that we can skip to it later
+					paramstart = ti+1;
+					break;
+				}
+				command[i]=buf[ti];
+				i++;
+			}
+
+			// search for an exclamation point
+			int excl_pos = strcspn( buf, "!" );
+
+			// USER MESSAGE
+			// if there is a !, and the ! is before the first space, then the message is being sent by someone
+			if (buf[excl_pos] && firstspace_pos > excl_pos)
+			{
+				// MAXNICKLEN is 30
+				char from[30];
+				
+				for(int ti=1;ti<excl_pos;++ti)
+				{
+					from[ti-1]=buf[ti];
+				}
+				from[excl_pos-1] = '\0';
+
+				// bypass everything that has just been parsed
+				buf += paramstart;
+				// buf should now only have parameters in it
+				
+				// PRIVMSG
+				//		:squeek!~squeek502@squeek.user.gamesurge PRIVMSG #FortressForever :test
+				//		:squeek!~squeek502@squeek.user.gamesurge PRIVMSG squeektest :hello
+				if (Q_strcmp(command,"PRIVMSG") == 0)
+				{
+					// going to a channel
+					if (buf[0] == '#')
+					{
+						// #fortressforever
+						if (Q_strnicmp((buf+1), "fortressforever", 15) == 0)
+						{
+							if (m_pLobbyTab)
+							{
+								char *msg = strchr(buf,':') + 1;
+
+								m_pLobbyTab->UserMessage(from, msg);
+								return;
+							}
+						}
+					}
+					// going to the local user
+					else if (Q_strncmp(buf, irc_user.nick, Q_strlen(irc_user.nick)) == 0)
+					{
+						if (m_pLobbyTab)
+						{
+							char *msg = strchr(buf,':') + 1;
+
+							m_pLobbyTab->SystemMessage(VarArgs("Private message from %s: %s", from, msg));
+							return;
+						}
+					}
+					else 
+					{
+						DevMsg("[IRC] Strange PRIVMSG syntax: %s", buf);
+					}
+				}
+				// QUIT
+				//		:squeek!~squeek502@squeek.user.gamesurge QUIT :Reason
+				else if (Q_strcmp(command,"QUIT") == 0)
+				{
+					// need to update all open tabs
+					return;
+				}
+				// JOIN
+				//		:squeek!~squeek502@squeek.user.gamesurge JOIN #FortressForever
+				else if (Q_strcmp(command,"JOIN") == 0)
+				{
+					// just to make sure a channel is being joined
+					if (buf[0] == '#')
+					{
+						// #fortressforever
+						if (Q_strnicmp((buf+1), "fortressforever", 15) == 0)
+						{
+							if (m_pLobbyTab)
+							{
+								m_pLobbyTab->UserList_AddUser(from);
+								return;
+							}
+						}
+					}
+				}
+				// PART
+				//		:squeek!~squeek502@squeek.user.gamesurge PART #FortressForever
+				else if (Q_strcmp(command,"PART") == 0)
+				{
+					// just to make sure a channel is being parted
+					if (buf[0] == '#')
+					{
+						// #fortressforever
+						if (Q_strnicmp((buf+1), "fortressforever", 15) == 0)
+						{
+							if (m_pLobbyTab)
+							{
+								m_pLobbyTab->UserList_RemoveUser(from);
+								return;
+							}
+						}
+					}
+				}
+			}
+			// SERVER MESSAGE
+			// if there is no !, or the space is before the !, then its a server message
+			else if( !buf[excl_pos] || (buf[excl_pos] && firstspace_pos < excl_pos) )
+			{
+				// don't need to parse the from, we know it's from the server
+				
+				// bypass everything that has just been parsed
+				buf += paramstart;
+				// buf should now only have parameters in it
+
+				// 353: Names list
+				//		:NuclearFallout.WA.US.GameSurge.net 353 squeektest = #FortressForever :squeektest Paft R00Kie GenghisTron Spartacus cjeshjoir- AtLarge mib_splib j3 []Zer0 Sailor_Mercury-sama_NL_id prodigy Hellfish`away @squeek @ChanServ +padawan
+				if (Q_strcmp(command,"353") == 0)
+				{
+					char *pszChan = strchr(buf,'#');
+					if (pszChan)
+					{
+						// #fortressforever
+						if (Q_strnicmp((pszChan+1), "fortressforever", 15) == 0)
+						{
+							if (m_pLobbyTab)
+							{
+								char *msg = strchr(pszChan,':') + 1;
+								
+								m_pLobbyTab->UserList_Fill(msg);
+								return;
+							}
+						}
+					}
+				}
+				// 366: End of names list
+				//		:Co-Locate.NL.EU.GameSurge.net 366 tempnick123 #FortressForever :End of /NAMES list.
+				else if (Q_strcmp(command,"366") == 0)
+				{
+					char *pszChan = strchr(buf,'#');
+					if (pszChan)
+					{
+						// #fortressforever
+						if (Q_strnicmp((pszChan+1), "fortressforever", 15) == 0)
+						{
+							if (m_pLobbyTab)
+							{
+								m_pLobbyTab->UserList_SetReceiving( false );
+								return;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+/*
+	// this is all incredibly insufficient; MODE can do things like +oovo nick1 nick2 nick3 nick4
+
+	// MODE
+	// :ChanServ!ChanServ@Services.GameSurge.net MODE #FortressForever +o squeek
+	if (strstr(buf,"MODE #") != NULL)
+	{
+		char *pszChan = strchr(buf,'#');
+		if (Q_strnicmp((pszChan+1), "fortressforever", 15) == 0)
+		{
+			// tell lobby list what happened
+			CFFIRCLobbyTab *tab = dynamic_cast <CFFIRCLobbyTab *> (m_pLobbyTab);
+			if (tab)
+			{
+				// user doing the changing
+				char user[40];
+				user[39] = '\0';
+				
+				for(int ti=1;ti<39;++ti)
+				{
+					if (buf[ti]=='!')
+					{
+						user[ti-1] = '\0';
+						break;
+					}
+					user[ti-1]=buf[ti];
+				}
+				
+				char *pszAccessChange = strchr(pszChan,' ')+1;
+
+				if (!pszAccessChange)
+					return;
+
+				char *pszUserChanged = strchr(pszAccessChange,' ')+1;
+
+				// channel mode being changed
+				if (!pszUserChanged)
+				{
+					tab->SystemMessage(VarArgs("%s sets channel mode: ", pszUserChanged, user));
+				}
+				// user mode being changed
+				else
+				{
+					if (pszAccessChange[0] == '+')
+					{
+						// getting vip'd
+						if (pszAccessChange[1] == 'v')
+						{
+							tab->UserList_UpdateUserAccess(pszUserChanged, 1);
+						}
+						// getting op'd
+						else if (pszAccessChange[1] == 'o')
+						{
+							tab->UserList_UpdateUserAccess(pszUserChanged, 2);
+						}
+						// getting banned
+						else if (pszAccessChange[1] == 'b')
+						{
+							tab->SystemMessage(VarArgs("%s was banned by %s", pszUserChanged, user));
+						}
+					}
+					else if (pszAccessChange[0] == '-')
+					{
+						// could be moving down
+						if (pszAccessChange[1] == 'v' || pszAccessChange[1] == 'o')
+						{
+							tab->UserList_UpdateUserAccess(pszUserChanged, 0);
+						}
+						else if (pszAccessChange[1] == 'b')
+						{
+							tab->SystemMessage(VarArgs("%s was unbanned by %s", pszUserChanged, user));
+						}
+					}
+				}
+
+				return;
+			}
+		}
+	}
+*/
+
+}
+
+//-----------------------------------------------------------------------------
+// CThreadTest implementation
+//-----------------------------------------------------------------------------
+
+CThreadTest::CThreadTest( void )
+{
+	SetName("IRCThread");
+	m_bIsRunning = false;
+	Start();
+}
+
+CThreadTest::~CThreadTest( void )
+{
+}
+
+int CThreadTest::Run()
+{
+	int a = 0;
+	//int i = 0;
+	char buf[3000];
+
+	m_bIsRunning = true;
+
+	while(IsAlive())
+	{
+		a = irc_socket.Recv(buf, sizeof(buf)-1);
+
+		// if length of message is bigger than 1
+		if (a > 1)
+		{
+			buf[a] = '\0';
+
+			//Msg("%s\n", buf);
+
+			int ichar =0;
+			char *p = buf;
+
+			// loop through each char
+			for (int t=0;t<(int)strlen(buf);t++)
+			{
+				// break at '\r' and send it off for parsing
+				if(p[ichar]=='\r')
+				{
+					p[ichar] = '\0';
+
+					if (g_pIRCPanel != NULL)
+						g_pIRCPanel->ParseServerMessage(p);
+					
+					p[ichar] = '\r';
+					// '\r' char is always followed by '\n'
+					p = (p+ichar+2);
+					ichar=0;
+					continue;
+				}
+				ichar++;
+			}
+
+			// send whatevers left
+			if (g_pIRCPanel != NULL)
+				g_pIRCPanel->ParseServerMessage(p);
+		}
+
+		a=0;
+	}
+
+	m_bIsRunning = false;
+/*
+	int i=0;
+	while(IsAlive())
+	{
+		i++;
+		Sleep(1000);
+		Msg("[IRCThread] %d...\n", i);
+	}*/
+
+	return 1;
+}
+
+CThreadTest& CThreadTest::GetInstance()
+{
+	static CThreadTest ircthread;
+	return ircthread;
 }
--- cl_dll/ff/vgui/ff_irc.h
+++ cl_dll/ff/vgui/ff_irc.h
@@ -12,17 +12,220 @@
 #include <vgui_controls/PropertyPage.h>
 #include <vgui_controls/TextEntry.h>
 #include <vgui_controls/RichText.h>
+#include <vgui_controls/ListPanel.h>
 #include "ff_gameui.h"
 
 using namespace vgui;
 
+//-----------------------------------------------------------------------------
+// Thread Test
+//-----------------------------------------------------------------------------
+
+class CThreadTest : public CThread
+{
+	DECLARE_CLASS_SIMPLE( CThreadTest, CThread );
+	
+	public:
+		int Run();
+		bool IsRunning() { return m_bIsRunning; };
+		static CThreadTest& GetInstance(); 
+		
+	protected:
+		CThreadTest( void );
+		~CThreadTest( void );
+
+	private:
+		bool m_bIsRunning;
+};
+
+//-----------------------------------------------------------------------------
+// IRC Tab
+//-----------------------------------------------------------------------------
 
 class CFFIRCTab : public vgui::PropertyPage
 {
 	DECLARE_CLASS_SIMPLE(CFFIRCTab, PropertyPage);
 
 public:
-	CFFIRCTab(Panel *parent, char const *panelName) : BaseClass(parent, panelName) {}
+	CFFIRCTab(Panel *parent, char const *panelName) : BaseClass(parent, panelName) 
+	{
+		m_pTextEntry_ChatEntry = new vgui::TextEntry(this, "TextEntry_ChatEntry");
+		m_pRichText_Chat = new vgui::RichText(this, "RichText_Chat");
+		m_pUserList = new ListPanel(this, "ListPanel_UserList");
+
+		m_pTextEntry_ChatEntry->SendNewLine(true); // Pressing enter in the text box triggers a new line message
+		m_pTextEntry_ChatEntry->AddActionSignalTarget( this ); // Something about sending messages to the right panel?
+		
+		m_pUserList->AddActionSignalTarget( this );
+		
+        m_pUserList->AddColumnHeader( 0, "access" , "" , 20/*, ListPanel::COLUMN_HIDDEN*/ );
+        m_pUserList->AddColumnHeader( 1, "name" , "User List" , 200 );
+
+		m_pUserList->SetSortFunc( 0, &(UserList_SortFunc) ); // access, name
+		m_pUserList->SetSortColumn( 0 ); // access
+		m_pUserList->SetColumnSortable( 1, false ); // turn off by-name-only sorting
+		
+		UserList_SetReceiving( false );
+	}
+
+	virtual void ApplySchemeSettings( IScheme *pScheme )
+	{
+		BaseClass::ApplySchemeSettings( pScheme );
+
+		m_pTextEntry_ChatEntry->SetFont( pScheme->GetFont( "DefaultVerySmall" ) );
+		m_pRichText_Chat->SetFont( pScheme->GetFont( "DefaultVerySmall" ) );
+		m_pUserList->SetFont( pScheme->GetFont( "DefaultVerySmall" ) );
+	}
+	
+	void UserMessage( const char *user, const char *message )
+	{
+		m_pRichText_Chat->InsertString( VarArgs("%s: %s\n", user, message) );
+		m_pRichText_Chat->GotoTextEnd();
+	}
+	
+	void SystemMessage( const char *message )
+	{
+		m_pRichText_Chat->InsertString( "< " );
+		m_pRichText_Chat->InsertString( message );
+		m_pRichText_Chat->InsertString( " >\n" );
+		m_pRichText_Chat->GotoTextEnd();
+	}
+
+	bool UserList_IsReceiving( ) { return m_bReceivingNames; }
+	void UserList_SetReceiving( bool bState ) { m_bReceivingNames = bState; }
+	
+	void UserList_Fill( const char *list )
+	{
+		// clear the list if this is the first names message
+		if (!UserList_IsReceiving())
+		{
+			UserList_SetReceiving( true );
+			m_pUserList->RemoveAll();
+		}
+
+		char *listBuffer = new char[ strlen(list) ];
+		Q_strcpy(listBuffer, list);
+
+		char *pUser = strtok(listBuffer, " \r\n");
+
+		// populate the list
+		while (pUser)
+		{
+			UserList_AddUser( pUser );
+
+			pUser = strtok(NULL, " \r\n");
+		}
+		delete[] listBuffer;
+
+		//m_pUserList->SortList();
+
+	}
+	
+	void UserList_AddUser( char *username )
+	{
+		KeyValues *kv = new KeyValues( "ircuser" );
+		switch(username[0])
+		{
+		// op
+		case '@':
+			kv->SetInt( "access", 2 );
+			username+=1;
+			break;
+		// vip
+		case '+':
+			kv->SetInt( "access", 1 );
+			username+=1;
+			break;
+		// regular user
+		default:
+			kv->SetInt( "access", 0 );
+		}
+		kv->SetString( "name", username );
+		// add and sort
+		m_pUserList->AddItem( kv, 0, false, true );
+		kv->deleteThis();
+
+		DevMsg("[IRC] Adding user to userlist: %s\n", username);
+	}
+
+	void UserList_RemoveUser( const char *username )
+	{
+		int index = UserList_FindUserByName( username );
+
+		if (m_pUserList->IsValidItemID(index))
+		{
+			m_pUserList->RemoveItem( index );
+			DevMsg("[IRC] Removing user from userlist: %s\n", username);
+		}
+	}
+	
+	void UserList_UpdateUserName( const char *username, const char *newname )
+	{
+		int index = UserList_FindUserByName( username );
+
+		if (m_pUserList->IsValidItemID(index))
+		{
+			KeyValues *kv = m_pUserList->GetItem( index );
+			DevMsg("[IRC] Updating user name from %s to: %s\n", username, newname);
+			kv->SetString( "name", newname );
+		}
+	}
+	
+	void UserList_UpdateUserAccess( const char *username, int newaccess )
+	{
+		int index = UserList_FindUserByName( username );
+
+		if (m_pUserList->IsValidItemID(index))
+		{
+			KeyValues *kv = m_pUserList->GetItem( index );
+			DevMsg("[IRC] Updating user access for %s to: %d\n", username, newaccess);
+			kv->SetInt( "access", newaccess );
+		}
+	}
+	
+	int UserList_FindUserByName( const char *username )
+	{
+		for( int i = m_pUserList->FirstItem(); m_pUserList->IsValidItemID(i); i = m_pUserList->NextItem(i) )
+		{
+			KeyValues *kv = m_pUserList->GetItem( i );
+			kv = kv->FindKey( "name" );
+			if( kv && Q_strcmp(kv->GetString(), username) == 0 )
+				return i;
+		}
+		return -1;
+	}
+
+protected:
+
+	static int UserList_SortFunc( ListPanel *pPanel, const ListPanelItem &item1, const ListPanelItem &item2 )
+	{
+		const vgui::ListPanelItem *p1 = &item1;
+		const vgui::ListPanelItem *p2 = &item2;
+
+		if ( !p1 || !p2 ) // No meaningful comparison
+			return 0;
+
+		// first compare access
+		int v1 = p1->kv->GetInt( "access" );
+		int v2 = p2->kv->GetInt( "access" );
+		if( v1 > v2 )
+			return -1;
+		else if( v1 < v2 )
+			return 1;
+
+		// next compare name
+		return Q_stricmp(p1->kv->GetString( "name" ), p2->kv->GetString( "name" )); 
+	}
+
+protected:
+
+	vgui::TextEntry			*m_pTextEntry_ChatEntry;
+	vgui::RichText			*m_pRichText_Chat;
+	vgui::ListPanel			*m_pUserList;
+
+private:
+	
+	bool m_bReceivingNames;
 
 };
 
@@ -36,9 +239,14 @@ class CFFIRCPanel : public Frame
 
 public:
 	CFFIRCPanel( vgui::VPANEL parent );
+	~CFFIRCPanel();
 
 	void AddGameTab();
 	void RemoveGameTab( CFFIRCTab *pTab );
+	
+	void ParseServerMessage( char *buf );
+
+	MESSAGE_FUNC( Close, "Close" );
 
 private:
 
@@ -51,11 +259,13 @@ private:
 	vgui::PropertySheet		*m_pIRCTabs;
 	//CFFIRCLobbyTab		*m_pLobbyTab;
 	//CFFIRCGameTab			*m_pGame1Tab;
-	vgui::PropertyPage		*m_pGameTab;
-	vgui::PropertyPage		*m_pLobbyTab;
+	CFFIRCTab				*m_pGameTab;
+	CFFIRCTab				*m_pLobbyTab;
 
 	vgui::TextEntry*		m_pTextEntry_ChatEntry;
-	vgui::RichText*		m_pRichText_LobbyChat;
+	vgui::RichText*			m_pRichText_LobbyChat;
+
+	CThreadTest				*m_pThread;
 
 	//vgui::Button			*m_pOKButton;
 	//vgui::Button			*m_pCancelButton;
