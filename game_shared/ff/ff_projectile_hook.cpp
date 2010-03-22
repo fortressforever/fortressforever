4bc7e8e46154d4e2d498d1670656c267cbc451bc
caesium <caesium@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2010-03-22 22:00:17 +0000

    ffdev_hook_angvel_cap: end hook if rotating around it too fast.

--- game_shared/ff/ff_projectile_hook.cpp
+++ game_shared/ff/ff_projectile_hook.cpp
@@ -24,11 +24,11 @@
 
 ConVar ffdev_hook_range( "ffdev_hook_range", "1000.0", FCVAR_REPLICATED | FCVAR_CHEAT, "Grappling hook range" );
 #define HOOK_RANGE ffdev_hook_range.GetFloat()
-ConVar ffdev_hook_closerange( "ffdev_hook_closerange", "200.0", FCVAR_REPLICATED | FCVAR_CHEAT, "Grappling hook close range" );
+ConVar ffdev_hook_closerange( "ffdev_hook_closerange", "0.0", FCVAR_REPLICATED | FCVAR_CHEAT, "Grappling hook close range" );
 #define HOOK_CLOSERANGE ffdev_hook_closerange.GetFloat()
 ConVar ffdev_hook_firespeed( "ffdev_hook_firespeed", "1500.0", FCVAR_REPLICATED | FCVAR_CHEAT, "Grappling hook fire speed" );
 #define HOOK_FIRESPEED ffdev_hook_firespeed.GetFloat()
-ConVar ffdev_hook_pullspeed( "ffdev_hook_pullspeed", "1000.0", FCVAR_REPLICATED | FCVAR_CHEAT, "Grappling hook pull speed" );
+ConVar ffdev_hook_pullspeed( "ffdev_hook_pullspeed", "650.0", FCVAR_REPLICATED | FCVAR_CHEAT, "Grappling hook pull speed" );
 #define HOOK_PULLSPEED ffdev_hook_pullspeed.GetFloat()
 
 #define ROPE_MATERIAL			"cable/rope_b.vmt"
@@ -55,7 +55,8 @@ ConVar ffdev_hook_rope_segments("ffdev_hook_rope_segments", "3", FCVAR_REPLICATE
 // caes: testing
 ConVar ffdev_hook_end_on_jump( "ffdev_hook_end_on_jump", "1", FCVAR_REPLICATED, "end hook if pressing jump and have ever had jump not pressed since last on ground" );
 ConVar ffdev_hook_swing( "ffdev_hook_swing", "1", FCVAR_REPLICATED, "enable swinging or original system" );
-ConVar ffdev_hook_pullspeed_falloff( "ffdev_hook_pullspeed_falloff", "0.5", FCVAR_REPLICATED, "how fast hook pull speed falls off as centripetal force increases (due to the winch's power)" );
+ConVar ffdev_hook_pullspeed_falloff( "ffdev_hook_pullspeed_falloff", "0.3", FCVAR_REPLICATED, "how fast hook pull speed falls off as centripetal force increases (due to the winch's power)" );
+ConVar ffdev_hook_angvel_cap( "ffdev_hook_angvel_cap", "3.0", FCVAR_REPLICATED, "end hook if rotating around it too fast" );
 // caes
 
 //#define PREDICTED_ROCKETS
@@ -405,6 +406,13 @@ void CFFProjectileHook::HookThink()
 			// calculate current speed in swing and radial directions
 			float flSwingSpeed = DotProduct( vecVel, vecSwingDir );
 			float flRadialSpeed = DotProduct( vecVel, vecPullDir );
+
+			// unscrew hook if angular velocity too high
+			if ( flSwingSpeed / flDistance > ffdev_hook_angvel_cap.GetFloat() )
+			{
+				RemoveHook();
+				return;
+			}
 
 			// centripetal force (mass is constant so force is proportional to accel)
 			float flCentripetalAccel = flSwingSpeed * flSwingSpeed / flDistance;
