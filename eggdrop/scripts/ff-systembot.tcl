33597a0d7b9ce2a53a4c39c0ec85a3ca30c4f813
Dexter <dexterhaslem@comcast.net>
2010-05-04 10:30:20 +0000

    use a simple assoc. array to keep track of games for now. !host returns !join #ffgame-#### incremented by the bot currently

--- eggdrop/scripts/ff-systembot.tcl
+++ eggdrop/scripts/ff-systembot.tcl
@@ -1,60 +1,61 @@
 #ff-systembot.tcl
 # this script updates clients on other client and game statuses in the global sys chan
 
-#dict set active_games ID channel players
 # todo: metatable or dictionary to keep track of current games easier
+# gameInfo = "$hostNick $ircchannel"
+set gameID 999
+set gameInfo($gameID) "dmh #ffgame-test"
+
 set ffbot(channel) "#ffirc-system"
-set gameid 1000
-set gamecount 0
 
 bind pub -|- !start start_game
 bind pub -|- !debug debugproc
 bind msg -|- !host parse_hostedgame
 
-# todo  - check gamenum is unique
 # DO NOT MOVE SHIT AROUND IN THIS FUNCTION - it will randomly stop working :(
 proc parse_hostedgame {nick host hand text} {
-	global gameid
 	global ffbot
-	global gamecount
+	global gameID
+	global gameInfo
 	
-	#set newchan "#ffgame-$text"
-	set newchan "#ffgame-$gameid"
+	incr gameID
+	set newchan "#ffgame-$gameID"
 	
+	set gameInfo($gameID) "$nick $newchan"
 	putquick "PRIVMSG $nick :!join $newchan"
-	putchan $ffbot(channel) "debug: new game created. irc channel = $newchan, hosted by $nick"
+	#putchan $ffbot(channel) "debug: new game created. irc channel = $newchan, hosted by $nick"
 	
 	# have the bot monitor the new chan for joins/parts and start
 	channel add $newchan
-	
-	incr gamecount
-	incr gameid
-	#set gamecount [incr gamecount]
-	#set gameid [incr gameid]
-
 	savechannels
 }
 
 # #ffgame-1234
 # gamenum is $chan 8:11
 proc start_game { nick mask handle chan text } {
-	global gamecount
-	global gameid
+	global gameInfo
 	set gamenum [string range $chan 8 11]
-	
 	putquick "PRIVMSG $chan :!connect $gamenum" 
-	incr gamecount -1
-	incr gameid -1 
-
-	putchan $ffbot(channel) "debug: game started! channel=$chan"
-	
+	#putchan $ffbot(channel) "debug: game started! channel=$chan"
+	unset gameInfo($gamenum)
 	channel remove $chan
+	savechannels
 }
 
 proc debugproc { nick mask handle chan text } {
-	global gamecount
-	global gameid
-	putchan $chan "Debug: gamecount = $gamecount, cur gameid=$gameid"
+	global gameInfo
+	global ffbot
+	
+	#putchan $ffbot(channel) "debug gameInfo#$text=$gameInfo($text)"
+	putchan $ffbot(channel) "debug: [array get gameInfo]"
+	
+	#set testID = [dict keys $gameInfo]
+	#dict for { ID host channel } $gameInfo {
+	#	putchan $ffbot(channel) "debug gameID=$ID host=$host channel=$channel"
+	#}
+	#putchan $ffbot(channel) "debug testID= $testID"
+	#putchan $ffbot(channel) "debug gameInfo#=$gameInfo"
+	#putchan $ffbot(channel) "debug gameID = [dict get $gameInfo id]"
 }
 
 putlog "SCRIPT: ff-systembot.tcl loaded"
