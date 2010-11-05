e1d23687b7064aa4d076da38550e88010357ee05
squeek <squeek502@hotmail.com>
2010-11-05 06:25:01 +0000

    Merge 12244 (Added a ultra basic training mode window.)

---
+++ cl_dll/ff/vgui/ff_training.h
@@ -0,0 +1,45 @@
+/********************************************************************
+	created:	2006/09/28
+	created:	28:9:2006   13:03
+	filename: 	f:\ff-svn\code\trunk\cl_dll\ff\vgui\ff_gamemodes.cpp
+	file path:	f:\ff-svn\code\trunk\cl_dll\ff\vgui
+	file base:	ff_gamemodes
+	file ext:	cpp
+	author:		Gavin "Mirvin_Monkey" Bramhill
+	
+	purpose:	
+*********************************************************************/
+
+#ifndef FF_TRAINING_H
+#define FF_TRAINING_H
+
+#ifdef _WIN32
+	#pragma once
+#endif
+
+#include "vgui_helpers.h"
+#include <vgui_controls/Frame.h>
+#include "ff_gameui.h"
+
+using namespace vgui;
+
+class CFFTrainingPanel : public Frame
+{
+	DECLARE_CLASS_SIMPLE(CFFTrainingPanel,Frame);
+
+public:
+	CFFTrainingPanel( vgui::VPANEL parent );
+
+
+private:
+
+	MESSAGE_FUNC_PARAMS(OnButtonCommand, "Command", data);
+
+	vgui::Button			*m_pOKButton;
+	vgui::Button			*m_pCancelButton;
+
+};
+
+DECLARE_GAMEUI(CFFTrainingUI, CFFTrainingPanel, fftraining);
+
+#endif // FF_TRAINING_H 
