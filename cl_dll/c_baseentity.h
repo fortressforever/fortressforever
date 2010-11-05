022705047bca372c973429d481f1602d4974a576
squeek <squeek.@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2010-11-05 04:39:11 +0000

    Merge 12027-12030 (LASER GRENADE: First Commit and some subsequent commits)

--- cl_dll/c_baseentity.h
+++ cl_dll/c_baseentity.h
@@ -85,6 +85,7 @@ enum Class_T
 	CLASS_GREN_GAS,		// Gas grenade
 	CLASS_GREN_CONC,	// Conc grenade
 	CLASS_GREN_CALTROP,	// Caltrop grenade
+	CLASS_GREN_LASER,
 	// END: Added by Mulchman
 
 	//-- Added by L0ki --
