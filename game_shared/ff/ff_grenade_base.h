09f1dc1201b5c6be6301835a55a4cc21b4713e6b
AfterShock <mikkey@gmail.com>
2010-12-04 18:56:39 +0000

    Added ability for grenades to set their own halo size, so we can reduce the huge ones and make the small ones a bit more visible on a per-grenade basis. Just set flModelSize in the constructor.
    Made mirvlet halos a bit bigger.

--- game_shared/ff/ff_grenade_base.h
+++ game_shared/ff/ff_grenade_base.h
@@ -108,10 +108,11 @@ protected:
 
 #else
 
-private:
+protected:
 
 	float	m_flModelSize;
-	
+
+private:	
 	// This is only needed client-side so far
 	virtual const unsigned char	*GetEncryptionKey() { return NULL; }
 	GRENADE_FILE_INFO_HANDLE	m_hGrenadeFileInfo;
