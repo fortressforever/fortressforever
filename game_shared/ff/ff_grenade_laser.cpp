44043c5ca5f28851df89b5803b8a8be86051a276
squeek <squeek.@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2011-03-29 02:57:35 +0000

    Merge 12712 (Setting the lasergren values in between Shock's and the old ones (angv from 66->160 to 120, damage from 660->250 to 350). The lasergren is meant to encourage dodging and therefore force predictable movement for the soli to exploit, so making the nade do low damage and move fast isn't the right direction to take it I don't think.)

--- game_shared/ff/ff_grenade_laser.cpp
+++ game_shared/ff/ff_grenade_laser.cpp
@@ -62,10 +62,10 @@ ConVar lasertime("ffdev_lasergren_time", "10", FCVAR_REPLICATED, "Laser active t
 
 #ifdef GAME_DLL
 
-	ConVar laserdamage("ffdev_lasergren_damage", "250", FCVAR_NOTIFY, "Damage of laser");
+	ConVar laserdamage("ffdev_lasergren_damage", "350", FCVAR_NOTIFY, "Damage of laser");
 	#define LASERGREN_DAMAGE_PER_TICK laserdamage.GetFloat()*gpGlobals->interval_per_tick
 	ConVar laserdamage_buildablemult("ffdev_lasergren_damage_buildablemult", ".15", FCVAR_NOTIFY, "Damage multiplier of laser against buildables");
-	ConVar laserangv("ffdev_lasergren_angv", "160", FCVAR_NOTIFY, "Laser angular increment");
+	ConVar laserangv("ffdev_lasergren_angv", "120", FCVAR_NOTIFY, "Laser angular increment");
 	#define LASERGREN_ROTATION_PER_TICK laserangv.GetFloat()*gpGlobals->interval_per_tick
 	ConVar laserjump( "ffdev_lasergren_jump", "80", FCVAR_NOTIFY, "Laser grenade jump distance" );
 	ConVar laserbob( "ffdev_lasergren_bob", "10", FCVAR_NOTIFY, "Laser grenade bob factor" );
