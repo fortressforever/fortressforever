74b331dd02fa193f68c508e1a7521bb1ee084c24
Dexter <Dexter@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2011-05-14 03:25:09 +0000

    sticking my stupid bash script here so i dont lose it later. should use a makefile :p

---
+++ linux_ff/auto_build_commit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "FF AUTOBUILD THING EL WOOTO"
+
+BINFILE=server_i486.so
+SRC_DIR=~/ffsrc
+SRC_BUILD_DIR=$SRC_DIR/linux_ff/
+MOD_DIR=~/ff_dev
+SVN_COMMIT_MSG="latest linux binary. built "`date +%x@%X`
+#echo $SVN_COMMIT_MSG
+
+cd $SRC_DIR
+svn up #&> /dev/null
+cd $SRC_BUILD_DIR
+make clean
+rm obj/ -rf
+export LD_LIBRARY_PATH=.
+make -j4 #&> /dev/null
+echo $?
+if [ $? = 0 ]; then
+	echo "Compiled successfully"	
+	pwd
+	#strip it to save ~5megs
+	strip $BINFILE
+	# svn commit the sucker
+	cp $BINFILE $MOD_DIR/bin && cd $MOD_DIR/bin
+	echo $?
+	svn commit $BINFILE -m "$SVN_COMMIT_MSG"
+	if [ $? = 0 ]; then
+		echo "Updated binary successfully, done!"
+	else
+		echo "Couldn't svn commit new bin for some reason"
+	fi
+else
+	echo "Couldn't compile for some reason!"
+fi
