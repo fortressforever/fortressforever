ea55f01e1c695f8d745a865cae72577a03383071
squeek <squeek.@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2011-06-05 02:30:52 +0000

    Forgot to make ffdev_headcrush vars tied to cheats

--- game_shared/gamemovement.cpp
+++ game_shared/gamemovement.cpp
@@ -67,8 +67,8 @@ bool g_bMovementOptimizations = true;	// |-- Mirv: Changed to false, but not sur
 static ConVar sv_sharkingfriction("sv_sharkingfriction", "1", FCVAR_REPLICATED | FCVAR_CHEAT);
 #define SV_SHARKINGFRICTION sv_sharkingfriction.GetFloat()
 
-static ConVar ffdev_headcrush_damage("ffdev_headcrush_damage", "108", FCVAR_REPLICATED, "Straight headcrush damage; not used if usefalldamage is on");
-static ConVar ffdev_headcrush_usefalldamage("ffdev_headcrush_usefalldamage", "1.0", FCVAR_REPLICATED, "0 = off, > 0 means take FALLDAMAGE * val damage");
+static ConVar ffdev_headcrush_damage("ffdev_headcrush_damage", "108", FCVAR_FF_FFDEV_REPLICATED, "Straight headcrush damage; not used if usefalldamage is on");
+static ConVar ffdev_headcrush_usefalldamage("ffdev_headcrush_usefalldamage", "1.0", FCVAR_FF_FFDEV_REPLICATED, "0 = off, > 0 means take FALLDAMAGE * val damage");
 
 #ifndef _XBOX
 void COM_Log( char *pszFile, char *fmt, ...)
