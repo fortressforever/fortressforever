16c58788af75d9c6dd899c61cc6e935008128db9
squeek <squeek.@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2011-06-26 21:31:35 +0000

    Merge 13242 (Jump pad health increased from 125 to 150)

--- dlls/ff/ff_mancannon.cpp
+++ dlls/ff/ff_mancannon.cpp
@@ -49,7 +49,7 @@ ConVar ffdev_mancannon_push_forward( "ffdev_mancannon_push_forward", "1024", FCV
 #define MANCANNON_PUSH_FORWARD ffdev_mancannon_push_forward.GetInt()
 ConVar ffdev_mancannon_push_up( "ffdev_mancannon_push_up", "512", FCVAR_FF_FFDEV_REPLICATED );
 #define MANCANNON_PUSH_UP ffdev_mancannon_push_up.GetInt()
-ConVar ffdev_mancannon_health( "ffdev_mancannon_health", "125", FCVAR_FF_FFDEV_REPLICATED );
+ConVar ffdev_mancannon_health( "ffdev_mancannon_health", "150", FCVAR_FF_FFDEV_REPLICATED );
 #define MANCANNON_HEALTH ffdev_mancannon_health.GetInt()
 ConVar ffdev_mancannon_health_regen( "ffdev_mancannon_health_regen", "20", FCVAR_FF_FFDEV_REPLICATED );
 #define MANCANNON_HEALTH_REGEN ffdev_mancannon_health_regen.GetFloat()
