d91254c92de351e887b6115e535d125d3a41da71
squeek <squeek.@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2011-12-11 01:55:08 +0000

    Merge 13513 (Fixed SpeedEffects getting cleared after they had already been cleared earlier in the function after the player_spawn Lua function was called. Max speed was not being recalculated after the redundant clear as well, so it'd still appear like you had the speed effect until another one overwrote it and recalced maxspeed.)

--- dlls/ff/ff_player.cpp
+++ dlls/ff/ff_player.cpp
@@ -1488,6 +1488,12 @@ void CFFPlayer::Spawn( void )
 	RemoveSolidFlags( FSOLID_NOT_SOLID );
 
 	SetupClassVariables();
+	
+	// equip the HEV suit
+	EquipSuit();
+
+	// Set on ground
+	AddFlag(FL_ONGROUND);
 
 	// Run this after SetupClassVariables in case lua is
 	// manipulating the players' inventory
@@ -1571,15 +1577,6 @@ void CFFPlayer::Spawn( void )
 	CFFLuaSC hFlagInfo;
 	hFlagInfo.Push(this);
 	_scriptman.RunPredicates_LUA(NULL, &hFlagInfo, "flaginfo");
-
-	for (int i=0; i<NUM_SPEED_EFFECTS; i++)
-		RemoveSpeedEffectByIndex( i );
-
-	// equip the HEV suit
-	EquipSuit();
-
-	// Set on ground
-	AddFlag(FL_ONGROUND);
 	
 	// Make sure we don't go running around during the intermission
 	// AfterShock - Commented out to try to prevent mapload respawn frozen
