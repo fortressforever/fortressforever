3f8c159508ebd6b8d10e8086ea0e49d99553a6c2
Dexter <dexterhaslem@comcast.net>
2012-01-10 05:02:02 +0000

    remove unused "Grenade.Timer" from precache dong

--- dlls/ff/ff_player.cpp
+++ dlls/ff/ff_player.cpp
@@ -866,7 +866,7 @@ void CFFPlayer::Precache()
 	PrecacheModel("models/player/civilian/civilian.mdl");
 
 	// Sounds
-	PrecacheScriptSound("Grenade.Timer");
+	//PrecacheScriptSound("Grenade.Timer");
 	PrecacheScriptSound("Grenade.Prime");
 	PrecacheScriptSound("Player.Jump");
 	PrecacheScriptSound("Player.SpyFall");
