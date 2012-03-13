16af566cf79ba52adc13378943ac6e15ef7af707
squeek <squeek502@hotmail.com>
2012-03-13 23:28:25 +0000

    - Made non-friendly players (hopefully) not block laser grenades when rising/bobbing

--- game_shared/ff/ff_grenade_laser.cpp
+++ game_shared/ff/ff_grenade_laser.cpp
@@ -266,6 +266,7 @@ float CFFGrenadeLaser::getLengthPercent()
 
 		// Should this maybe be noclip?
 		SetMoveType(MOVETYPE_FLY);
+		SetCollisionGroup(COLLISION_GROUP_NONE);
 	
 		m_bIsOn = true;
 		SetThink(&CFFGrenadeLaser::BeamEmit);
