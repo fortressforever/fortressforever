dc85d9f2f6f1e778c6f0b462e47e89574acd51a0
squeek <squeek.@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2012-06-10 23:31:31 +0000

    Merge 13771 (Fixed (hopefully) maps with flythroughs overriding the SourceTV free-roam view (making it impossible to move and forcing you periodically to jump to the next overview target))

--- cl_dll/ff/c_ff_player.cpp
+++ cl_dll/ff/c_ff_player.cpp
@@ -1694,7 +1694,7 @@ void C_FFPlayer::Death()
 void C_FFPlayer::CreateMove(float flInputSampleTime, CUserCmd *pCmd)
 {
 	// Mapguides
-	if (GetTeamNumber() == TEAM_SPECTATOR && m_hNextMapGuide)
+	if (!IsHLTV() && GetTeamNumber() == TEAM_SPECTATOR && m_hNextMapGuide)
 	{
 		pCmd->buttons = 0;
 		pCmd->forwardmove = 0;
@@ -2100,7 +2100,7 @@ const QAngle &C_FFPlayer::EyeAngles()
 		return m_angEyeAngles;
 
 	// Mapguides
-	if (GetTeamNumber() < TEAM_BLUE && m_hNextMapGuide)
+	if (!IsHLTV() && GetTeamNumber() < TEAM_BLUE && m_hNextMapGuide)
 	{
 		float t = clamp((m_flNextMapGuideTime - gpGlobals->curtime) / m_hLastMapGuide->m_flTime, 0, 1.0f);
 		t = SimpleSpline(t);
