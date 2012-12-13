a6e3d1181f8392dc2680faffc5942af3a8df962e
squeek <squeek502@hotmail.com>
2012-12-13 09:20:46 +0000

    I think this is all that's needed to make cloaked viewmodels show up when specing; needs to be tested

--- cl_dll/c_baseviewmodel.cpp
+++ cl_dll/c_baseviewmodel.cpp
@@ -271,7 +271,8 @@ int C_BaseViewModel::DrawModel( int flags )
 	if ( ShouldFlipViewModel() )
 		materials->CullMode( MATERIAL_CULLMODE_CW );
 
-	C_BasePlayer *pPlayer = C_BasePlayer::GetLocalPlayer();
+	C_FFPlayer *pPlayer = ToFFPlayer( GetOwner() );
+	//C_BasePlayer *pPlayer = C_BasePlayer::GetLocalPlayer();
 	int ret;
 	// If the local player's overriding the viewmodel rendering, let him do it
 	// Jon: override if we have an override material
