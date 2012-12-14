8b536e8bfeac135f407c750f577239f311c9274f
squeek <squeek502@hotmail.com>
2012-12-14 02:31:05 +0000

    - Fixed flag shadow being drawn when first-person specing someone while they have the flag - Used GetLocalFFPlayerOrObserverTarget to clean up the DrawModel logic

--- cl_dll/ff/c_ff_item_flag.cpp
+++ cl_dll/ff/c_ff_item_flag.cpp
@@ -111,21 +111,12 @@ int C_FFInfoScript::DrawModel( int flags )
 	if( input->CAM_IsThirdPerson() )
 		return BaseClass::DrawModel( flags );
 
-	if (GetFollowedEntity() == CBasePlayer::GetLocalPlayer()) 
-		return 0;
-
-	CBasePlayer *pLocalPlayer = CBasePlayer::GetLocalPlayer();
-
 	// Bug #0001660: Flag visible in first-person spectator mode
 	// Don't draw the FFInfoScript if we're in-eye spectating the same player the flag is hitched to |---> Defrag
-	if( pLocalPlayer )
-	{
-        if( pLocalPlayer->IsObserver() && ( pLocalPlayer->GetObserverMode() == OBS_MODE_IN_EYE ))		
-		{
-			if( pLocalPlayer->GetObserverTarget() == GetFollowedEntity() )
-				return 0;
-		}
-	}
+	CBasePlayer *pLocalPlayer = C_FFPlayer::GetLocalFFPlayerOrObserverTarget();
+
+	if (pLocalPlayer && pLocalPlayer == GetFollowedEntity())
+		return 0;
 
 	// Temporary fix until we sort out the interpolation business
 	if (m_flThrowTime + 0.2f > gpGlobals->curtime) 
@@ -146,7 +137,7 @@ ShadowType_t C_FFInfoScript::ShadowCastType( void )
 	if( !ShouldDraw() )
 		return SHADOWS_NONE;
 
-	if( GetFollowedEntity() == C_BasePlayer::GetLocalPlayer() )
+	if( GetFollowedEntity() == C_FFPlayer::GetLocalFFPlayerOrObserverTarget() )
 	{
 		if( r_selfshadows.GetInt() )
 			return SHADOWS_RENDER_TO_TEXTURE;
