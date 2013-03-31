fa8f2b867f07a67c6c6c422b6a4bdc4a28d6e818
AfterShock <mikkey@gmail.com>
2013-03-31 19:27:52 +0000

    Made DevMsg function which prepends "SERVER" or "CLIENT" depending on which side it's running on, to debug client vs server side lag stuff

--- game_shared/ff/ff_utils.cpp
+++ game_shared/ff/ff_utils.cpp
@@ -1398,3 +1398,13 @@ void FF_SendStopGrenTimerMessage(CFFPlayer *target)
 
 #endif
 
+void FF_DevMsg(const char *pMsg)
+{
+#ifdef GAME_DLL
+	DevMsg("SERVER: %s",pMsg);
+#endif
+
+#ifdef CLIENT_DLL
+	DevMsg("CLIENT: %s",pMsg);
+#endif
+}
