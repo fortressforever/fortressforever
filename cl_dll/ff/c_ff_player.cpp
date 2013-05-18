ee4824cb07e189da837d47c4d08864f5825cee62
squeek <squeek.@d5e3c355-140c-0410-b1fe-a40c9a77c12c>
2013-05-18 03:12:53 +0000

    Merge 14261 (Reverted r13949 (cl_ragdolltime fix)  - This was causing CL_CopyExistingEntity errors, need a more robust fix (make the ragdolls able to determine when they should remove themselves rather than the server dictating that))

--- cl_dll/ff/c_ff_player.cpp
+++ cl_dll/ff/c_ff_player.cpp
@@ -1010,9 +1010,6 @@ static ConVar cl_ragdolltime("cl_ragdolltime", "25.0", FCVAR_ARCHIVE);
 
 void C_FFRagdoll::CreateRagdoll()
 {
-	if (!cl_ragdolltime.GetBool())
-		return;
-
 	// First, initialize all our data. If we have the player's entity on our client,
 	// then we can make ourselves start out exactly where the player is.
 	C_FFPlayer *pPlayer = dynamic_cast< C_FFPlayer* >( m_hPlayer.Get() );
@@ -1072,7 +1069,7 @@ void C_FFRagdoll::CreateRagdoll()
 		// We can also spawn a valid weapon
 		if (pWeapon && pWeapon->GetWeaponID() < FF_WEAPON_DEPLOYDISPENSER)
 		{
-			C_Gib *pGib = C_Gib::CreateClientsideGib(pWeapon->GetFFWpnData().szWorldModel, pWeapon->GetAbsOrigin(), GetAbsVelocity(), Vector(0, 0, 0), cl_ragdolltime.GetFloat());
+			C_Gib *pGib = C_Gib::CreateClientsideGib(pWeapon->GetFFWpnData().szWorldModel, pWeapon->GetAbsOrigin(), GetAbsVelocity(), Vector(0, 0, 0), 10.0f);
 
 			if (pGib)
 			{
@@ -1124,7 +1121,6 @@ void C_FFRagdoll::CreateRagdoll()
 	SetSolid(SOLID_BBOX);
 	AddSolidFlags(FSOLID_NOT_STANDABLE);
 	SetCollisionGroup(COLLISION_GROUP_WEAPON);
-	SetNextClientThink( gpGlobals->curtime + cl_ragdolltime.GetFloat() );
 
 	m_pBloodStreamEmitter = CBloodStream::Create(this, "BloodStream");
 	m_pBloodStreamEmitter->SetDieTime(gpGlobals->curtime + cl_ragdolltime.GetFloat());
@@ -1147,8 +1143,7 @@ void C_FFRagdoll::OnDataChanged( DataUpdateType_t type )
 			pPhysicsObject->AddVelocity( &vecExaggeratedVelocity, &aVelocity );
 		}
 
-		// no longer think always, only think when it should be removed
-		//SetNextClientThink( CLIENT_THINK_ALWAYS );
+		SetNextClientThink( CLIENT_THINK_ALWAYS );
 	}
 }
 
@@ -1185,10 +1180,6 @@ IRagdoll* C_FFPlayer::GetRepresentativeRagdoll() const
 //-----------------------------------------------------------------------------
 void C_FFRagdoll::ClientThink( void )
 {
-	// This ragdoll only thinks at the end of its lifetime
-	Release();
-	return;
-
 	/*
 	if( UTIL_PointContents( GetAbsOrigin() ) & MASK_WATER )
 	{
