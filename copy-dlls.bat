5b9383a5c2dd2d3de140c0e2587c883195a9f1e7
Mike Parker <mikkey@gmail.com>
2014-10-25 15:54:15 +0100

    batch file to copy dlls into steam dir, will need editing on most peoples machines i guess

---
+++ copy-dlls.bat
@@ -0,0 +1,3 @@
+copy "cl_dll\Debug FF\2005\client.dll" "D:\Steam\SteamApps\common\Fortress Forever\FortressForever\bin\client.dll"
+copy "dlls\Debug FF\2005\server.dll" "D:\Steam\SteamApps\common\Fortress Forever\FortressForever\bin\server.dll"
+pause
