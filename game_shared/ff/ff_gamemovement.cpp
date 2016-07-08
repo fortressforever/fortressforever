1926504dea33f697b85c58fefdec63c051e74b03
Mike Parker <mikkey@gmail.com>
2016-07-08 23:45:10 +0100

    Remove the "flames extinguish if you move fast" - it triggers accidentally on people you are fighting sometimes and if an offy class concs away, the pyro wont be able to catch up anyway

--- game_shared/ff/ff_gamemovement.cpp
+++ game_shared/ff/ff_gamemovement.cpp
@@ -31,7 +31,6 @@
 // When changing jump height, recalculate the FF_MUL_CONSTANT!!!
 #define FF_JUMP_HEIGHT 27.5f // Modified by Mulch 10/20/2005 so we could jump on 63 but not 64 unit high stuff
 #define FF_MUL_CONSTANT 209.76177f //sqrt(2.0f * 800.0f * FF_JUMP_HEIGHT);
-#define EXTINGUISH_FIRE_SPEED 700.0f
 //static ConVar FF_JUMP_HEIGHT( "ffdev_jump_height", "27.5", FCVAR_FF_FFDEV );
 
 //static ConVar sv_trimpmultiplier("sv_trimpmultiplier", "1.4", FCVAR_REPLICATED | FCVAR_CHEAT);
@@ -921,12 +920,7 @@ void CFFGameMovement::CheckVelocity( void )
 		return;
 
 	pPlayer->SetRampsliding(IsRampSliding(pPlayer));
-#ifdef GAME_DLL
-	if ( pPlayer->GetMovementSpeed() > EXTINGUISH_FIRE_SPEED)
-	{
-		pPlayer->Extinguish();
-	}
-#endif
+
 	if( !pPlayer->IsCloaked() )
 		return;
 
